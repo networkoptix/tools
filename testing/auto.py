400a661ec94b37083971f26e7e9e229e765b094d
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2015-07-31 19:46:45 +0300

    Performs hg pull, update (to versions specified), build, unit tests build and run unit tests
    <==== Mercurial data ====>
    Branch: master

--- testing/auto.py
+++ testing/auto.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 #TODO: ADD THE DESCRIPTION!!!
 import sys, os, os.path, time, re
-from subprocess import Popen, PIPE, STDOUT
+import subprocess
+from subprocess import Popen, PIPE, STDOUT, CalledProcessError
+from collections import deque
+import errno
 import select
 import traceback
 from smtplib import SMTP
@@ -22,9 +25,32 @@ READY = select.POLLIN | select.POLLPRI
 ToSend = []
 Env = os.environ.copy()
 
+DEBUG = 1
+
+
+def log_print(s):
+    print s
 
 def log(text):
-    print "[%s] %s" % (time.strftime("%Y:%m:%d %X %Z"), text)
+    log_print("[%s] %s" % (time.strftime("%Y:%m:%d %X %Z"), text))
+
+
+def debug(text, *args):
+    if DEBUG:
+        if args:
+            log_print("DEBUG: " + (text % args))
+        else:
+            log_print("DEBUG: " + text)
+
+
+def email_send(mailfrom, mailto, msg):
+    msg['From'] = mailfrom
+    msg['To'] = mailto
+    smtp = SMTP('localhost')
+    #smtp.set_debuglevel(1)
+    smtp.sendmail(mailfrom, mailto, msg.as_string())
+    smtp.quit()
+
 
 def email_notify(lines):
     msg = MIMEText.MIMEText(
@@ -32,13 +58,17 @@ def email_notify(lines):
         ("\n\n[Finished at: %s]" % time.strftime("%Y.%m.%d %H:%M:%S (%Z)"))
     )
     msg['Subject'] = "Autotest run results"
-    msg['From'] = MAIL_FROM
-    msg['To'] = MAIL_TO
-    smtp = SMTP('localhost')
-    #smtp.set_debuglevel(1)
-    smtp.sendmail(MAIL_FROM, MAIL_TO, msg.as_string())
-    smtp.quit()
+    email_send(MAIL_FROM, MAIL_TO, msg)
 
+
+def email_build_error(branch, loglines, crash=False):
+    cause = "Error building branch %s" if not crash else ("Branch %s build crashes!" + ("Traceback: "))
+    msg = MIMEText.MIMEText(
+        ("%s\nThe build log last %d lines are:\n" % (cause, len(loglines))) +
+        "\n".join(loglines) + "\n"
+    )
+    msg['Subject'] = "Autotest scriprt fails to build the branch " + branch
+    email_send(MAIL_FROM, MAIL_TO, msg)
 
 def get_name(line):
     m = NameRx.match(line)
@@ -113,12 +143,13 @@ def perfom_test(poller, proc):
 
 
 def call_test(testname, poller):
+    debug("[ Calling %s tests ]" % testname)
     ToSend.append("[ Calling %s tests ]" % testname)
     old_len = len(ToSend)
     proc = None
     try:
-        proc = Popen([os.path.join(BIN_PATH, testname)], bufsize=0, stdout=PIPE, stderr=STDOUT, cwd=PROJECT_ROOT,
-                     env=Env, universal_newlines=True)
+        proc = Popen([os.path.join(BIN_PATH, testname)], bufsize=0, stdout=PIPE, stderr=STDOUT,
+                    env=Env, **SUBPROC_ARGS)
         #print "Test is started with PID", proc.pid
         poller.register(proc.stdout, READ_ONLY)
         perfom_test(poller, proc)
@@ -136,12 +167,14 @@ def call_test(testname, poller):
     finally:
         if proc: poller.unregister(proc.stdout)
         if len(ToSend) == old_len:
+            debug("No intereating output from these tests")
             del ToSend[-1]
         else:
             ToSend.append('')
 
 
-def run_tests():
+def run_tests(branch):
+    log("Running unit tests for branch %s" % branch)
     ToSend = []
     poller = select.poll()
 
@@ -149,26 +182,115 @@ def run_tests():
         call_test(name, poller)
 
     if ToSend:
+        debug("Tests output:\n" + "\n".join(ToSend))
         email_notify(ToSend)
 
 
-def check_new_commits():
+def filter_branch_names(branches):
+    "Check names for exact eq with list, drop duplicates"
+    # The problem is `hg in --branch` takes all branches with names beginning with --branch value. :(
+    filtered = []
+    for name in branches:
+        if name in BRANCHES and not name in filtered:
+            filtered.append(name)
+            # hope it wont be used for huge BRANCHES list
+    return filtered
+
+
+def check_new_commits(bundle_fn):
     "Check the repository for new commits in the controlled branches"
     log("Check for new commits")
-    
+    try:
+        ready_branches = subprocess.check_output(HG_IN + ['--bundle', bundle_fn], **SUBPROC_ARGS)
+        if ready_branches:
+            debug("Commits found in branches: %s", ready_branches)
+            branches = filter_branch_names(ready_branches.split(','))
+            debug("Filtered branches: %s", branches)
+            if branches:
+                return branches
+    except CalledProcessError, e:
+        if e.returncode != 1:
+            debug("hg in call returns %s code. Output:\n%s", e.returncode, e.output)
+            raise
+    log("No new commits found for controlled branches.")
+    return []
+
+
+def update_repo(branches, bundle_fn):
+    log("Pulling branches %s" % (', '.join(branches)))
+    debug("Using bundle file %s", bundle_fn)
+    #try:
+    subprocess.check_call(HG_PULL + [bundle_fn], **SUBPROC_ARGS)
+    #except CalledProcessError, e:
+
+
+def call_maven_build(branch, unit_tests=False):
+    last_lines = deque(maxlen=BUILD_LOG_LINES)
+    log("Build netoptix_vms...")
+    kwargs = SUBPROC_ARGS
+    try:
+        if unit_tests:
+            kwargs = kwargs.copy()
+            kwargs["cwd"] = os.path.join(kwargs["cwd"], UT_SUBDIR)
+            branch += ' unit tests'
+        log("Build branch %s..." % branch)
+        proc = Popen([MVN, "package"], bufsize=50000, stdout=PIPE, stderr=STDOUT, **kwargs)
+        for line in proc.stdout:
+            last_lines.append(line)
+            print ":: " + line,
+            #if proc.poll() is not None:
+            #    break
+        if proc.poll() is None:
+            debug("Maven call has stoped print lines but isn't terminated yet")
+            proc.terminate()
+        if proc.returncode != 0:
+            log("Error calling maven")
+            log("The last %d log lines:" % len(last_lines))
+            log_print("".join(last_lines))
+            email_build_error(branch, last_lines)
+            return False
+    except CalledProcessError:
+        tb = traceback.format_exc()
+        log("maven call has failed: %s" % tb)
+        log("The last %d log lines:" % len(last_lines))
+        log_print("".join(last_lines))
+        email_build_error(branch, last_lines, crash=tb)
+        return False
+    return True
+
+
+def prepare_branch(branch):
+    log("Switch to the banch %s" % branch)
+    subprocess.check_call(HG_PURGE, **SUBPROC_ARGS)
+    subprocess.check_call(HG_UP + [branch], **SUBPROC_ARGS)
+    return call_maven_build(branch) and call_maven_build(branch, unit_tests=True)
+
 
 def perform_check():
     "Check for repository updates, get'em, build and test"
-    run_tests()
+    bundle_fn = os.path.join(TEMP, "in.hg")
+    branches = check_new_commits(bundle_fn)
+    if branches:
+        update_repo(branches, bundle_fn)
+        for branch in branches:
+            if prepare_branch(branch):
+                run_tests(branch)
 
 
 
 def run():
     log("Starting...")
-    old_cwd = os.getcwd()
-    if old_cwd != PROJECT_ROOT:
-        os.chdir(PROJECT_ROOT)
-        log("Switched to the project directory %s" % PROJECT_ROOT)
+#    start_cwd = os.getcwd()
+#    if TEMP == '':
+#        global TEMP
+#        TEMP = start_cwd
+#    if old_cwd != PROJECT_ROOT:
+#        os.chdir(PROJECT_ROOT)
+ #       log("Switched to the project directory %s" % PROJECT_ROOT)
+    if not os.path.isdir(PROJECT_ROOT):
+        raise EnvironmentError(errno.ENOENT, "The project root directory not found", PROJECT_ROOT)
+    if not os.access(PROJECT_ROOT, os.R_OK|os.W_OK|os.X_OK):
+        raise IOError(errno.EACCES, "Full access to the project root directory required", PROJECT_ROOT)
     log("Watched branches: " + ','.join(BRANCHES))
 
     if Env.get('LD_LIBRARY_PATH'):
--- testing/testconf.py
+++ testing/testconf.py
@@ -2,11 +2,15 @@ __author__ = 'Danil Lavrentyuk'
 """
 The main configuration file for (auto-)test scripts.
 """
-import os.path
+import os, os.path
 
-PROJECT_ROOT = "~/develop/netoptix_vms"
+PROJECT_ROOT = "~/develop/nvms-tmp"
+TEMP = '' # temporary files directory, leave it '' for the process' current directory
 
 ######################################################################
+if TEMP == '':
+    TEMP = os.getcwd()
+
 if PROJECT_ROOT.startswith('~'):
     PROJECT_ROOT = os.path.expanduser(PROJECT_ROOT)
 
@@ -18,13 +22,30 @@ LIB_PATH = os.path.join(TARGET_PATH, "lib/release")
 ######################################################################
 
 
-HG_CHECK_PERIOD = 3 * 60 # seconds, complete check period, including time consumed by check, builds and tests
-MIN_SLEEP = 30 # seconds, minimal sleep time after one perform before another
+HG_CHECK_PERIOD = 5 * 60 # seconds, complete check period, including time consumed by check, builds and tests
+MIN_SLEEP = 60 # seconds, minimal sleep time after one perform before another
 PIPE_TIMEOUT = 10 * 1000  # milliseconds
+BUILD_LOG_LINES = 100 # How may last lines are saved to report build process failure
 
 BRANCHES = ('dev_2.4.0', 'dev_2.5', 'prod_2.3.2')
+BRANCHES = ('dev_2.4.0',)
 TESTS = ('common_ut', 'mediaserver_core_ut')
+UT_SUBDIR = "unit_tests"
 
 MAIL_FROM = "autotest-script"
 MAIL_TO = 'dlavrentyuk@networkoptix.com'
 
+#TODO
+ALERT_TO = 'dlavrentyuk@networkoptix.com' # to send mail about check process fails (such as hg call fails)
+
+#############################################
+
+HG = "/usr/bin/hg"
+MVN = "/home/danil/develop/buildenv/maven/bin/mvn"
+
+HG_IN = [HG, "incoming", "--quiet", "--template={branch},"] + [ "--branch=%s" % b for b in BRANCHES ]
+HG_PULL = [HG, "pull", "--quiet"]
+HG_UP = [HG, "update", "--rev"]
+HG_PURGE = [HG, "purge", "--all"]
+
+SUBPROC_ARGS = dict(universal_newlines=True, cwd=PROJECT_ROOT, shell=False)
