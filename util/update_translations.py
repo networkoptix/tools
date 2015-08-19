bc03ea1dd9ca745fe52fce4f2c5a45d20f353cef
Sergey Ivanov <sivanov@networkoptix.com>
2015-08-19 15:25:10 +0300

    ENV-28: Improve "update_translations" script
    <==== Mercurial data ====>
    Branch: master

--- util/update_translations.py
+++ util/update_translations.py
@@ -1,12 +1,22 @@
+# -*- coding: utf-8 -*-
 #/bin/python
 
 import sys
 import os
 import argparse
+from multiprocessing import Process
 import threading
 import subprocess
+from common_module import init_color,info,green,warn,err,separator
 
-projects = ['common', 'client', 'traytool']
+projects = ['common', 'traytool', 'client']
+ignored = [
+            'qstringbuilder.h', 'qstring.h', 'qmatrix.h', 'qaction.h', 'qnetworkcookiejar.h',
+            '/boost', '/libavutil', '/openssl', 
+            '.prf', '.pro(1)'
+          ]
+verbose = False
+results = dict()
 
 def update(project, translationDir, projectFile):
     entries = []
@@ -28,10 +38,44 @@ def update(project, translationDir, projectFile):
     command = 'lupdate -no-obsolete -no-ui-lines -pro ' + projectFile + ' -locations none -ts'
     for path in entries:
         command = command + ' ' + path
-    print command
-    subprocess.call(command)
+    if verbose:
+        info(command)
+        separator()
+    log = subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
+    global results
+    results[project] = log
 
+def handleOutput(log):
+    for line in log.split('\n'):
+        line = line.strip('\n')
+        if len(line) == 0:
+            continue;
+        
+        skipLine = False
+        for s in ignored:
+            if s in line:
+                skipLine = True
+        if skipLine:
+            continue;
+                
+        if "Discarding" in line:
+            warn(line)
+        elif "lacks" in line:
+            err(line)
+        elif verbose:
+            info(line)
+            
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help="verbose output")
+    parser.add_argument('-c', '--color', action='store_true', help="colorized output")
+    args = parser.parse_args()
+    global verbose
+    verbose = args.verbose
+
+    if args.color:
+        init_color()
+
     rootDir = os.getcwd()
     
     threads = []
@@ -39,6 +83,7 @@ def main():
         projectDir = os.path.join(rootDir, project)
         translationDir = os.path.join(projectDir, 'translations')
         projectFile = os.path.join(projectDir, 'x64/' + project + '.pro')
+        #thread = Process(target=update, args=(project, translationDir, projectFile))
         thread = threading.Thread(None, update, args=(project, translationDir, projectFile))
         thread.start()
         threads.append(thread)
@@ -46,8 +91,10 @@ def main():
     for thread in threads:
         thread.join()
         
-    print "ok"
-    
+    for project in projects:
+        separator()
+        handleOutput(results[project])
+   
     
 if __name__ == "__main__":
     main()
