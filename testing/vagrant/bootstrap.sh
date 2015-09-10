4c89d9beef1a1b68a28ee57c1bf167e436046b0d
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2015-09-10 20:03:40 +0300

    Files to support test virtual machines creation.
    <==== Mercurial data ====>
    Branch: master

---
+++ testing/vagrant/bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+_DEB=/vagrant/networkoptix-mediaserver-2.4.0.0-x64-release-beta.deb
+_CONF=/opt/networkoptix/mediaserver/etc/mediaserver.conf
+
+. /vagrant/fix-hostname.sh $1
+DEBIAN_FRONTEND=noninteractive dpkg -i --force-depends "$_DEB"
+#apt-get install -f --yes --allow-unauthenticated
+
+stop networkoptix-mediaserver
+#echo '...Before:'
+#cat "$_CONF"
+sed -i 's/^appserverPassword\s*=.*/appserverPassword=123/' "$_CONF"
+if grep -q '^systemName' "$_CONF"; then
+	sed -i 's/^systemName\s*=.*/systemName=functesting/' "$_CONF"
+else
+	sed -i 's/^\[General\].*/\0\nsystemName=functest/' "$_CONF"
+fi
+#echo '...After:'
+#cat "$_CONF"
+start networkoptix-mediaserver
+
+
