5be7ea1a9c96dd3a0ebb6ac6375b788aa5126288
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2015-10-28 18:04:31 +0300

    added
    <==== Mercurial data ====>
    Branch: master

---
+++ testing/backuptest.py
@@ -0,0 +1,13 @@
+__author__ = 'Danil Lavrentyuk'
+__author__ = 'Danil Lavrentyuk'
+import urllib2
+import unittest
+import subprocess
+import traceback
+import time
+import os
+import json
+import socket
+import struct
+
+from functest_util import ClusterLongWorker, SafeJsonLoads, get_server_guid
---
+++ testing/testboxes.py
@@ -0,0 +1,3 @@
+"""Contains common tools to handle virtual boxes based mediaserver functional testing.
+"""
+__author__ = 'Danil Lavrentyuk'
