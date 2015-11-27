e1238356a5526787454c1c89cac15d9c6b28d0c9
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2015-11-27 16:39:23 +0300

    the problem with checkin comment possibly fixed
    <==== Mercurial data ====>
    Branch: master

--- util/merge_dev.py
+++ util/merge_dev.py
@@ -12,28 +12,37 @@ verbose = False
 header = 'Merge Changelog:'
 
 def getCurrentBranch():
-    command = 'hg branch'
-    return subprocess.check_output(command, shell=True).strip('\n')
+    return subprocess.check_output(['hg', 'branch']).strip('\n')
 
-def execCommand(command):
+def execCommand(*command):
     if verbose:
         print command
     
-    code = subprocess.call(command, shell=True)
+    code = subprocess.call(command)
     if code != 0:
+        print "Subprocess returned code {0}. Terminating...".format(code)
         sys.exit(code)
     return code
         
 def getChangelog(revision):
-    command = 'hg log -T "{desc}\\n\\n" -r '
-    changeset = '"(::{0} - ::{1})"'.format(revision, targetBranch)
+    command = ['hg', 'log', '--template', '{desc}\n\n', '-r']
+    changeset = ["(::{0} - ::{1})".format(revision, targetBranch)]
     command = command + changeset
-    changelog = subprocess.check_output(command, shell=True)
+    try:
+        changelog = subprocess.check_output(command, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError, e:
+        print "Command {0} execution failed with return code {1}".format(e.cmd, e.returncode)
+        print "Command's output:"
+        print e.output
+        return ''
     changes = sorted(set(changelog.split('\n\n')))
     changes = [x.strip('\n').replace('"', '\'') for x in changes if 
         not x in ignoredCommits and not x.startswith(header)]
-    
-    changes.insert(0, 'Merge Changelog:')
+
+    if changes:
+        changes.insert(0, 'Merge Changelog:')
+    else:
+        return 'Merge Changelog is empty!'
     
     return '\n'.join(changes).strip('\n')
       
@@ -70,10 +79,10 @@ def main():
     if revision == '.' and targetBranch != currentBranch:
         revision = currentBranch
    
-    execCommand('hg up {0}'.format(targetBranch))
-    execCommand('hg merge  --tool=internal:merge {0}'.format(revision))
-    execCommand('hg ci -m"{0}"'.format(getChangelog(revision)))
-    execCommand('hg up {0}'.format(currentBranch))
+    execCommand('hg', 'up', targetBranch)
+    execCommand('hg', 'merge',  '--tool=internal:merge', revision)
+    execCommand('hg', 'ci', '-m' + getChangelog(revision))
+    execCommand('hg', 'up', currentBranch)
     sys.exit(0)
     
 if __name__ == "__main__":
