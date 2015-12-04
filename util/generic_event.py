5a286c4f80a48d8b886fa1241c76ca5afca004c5
Sergey Ivanov <sivanov@networkoptix.com>
2015-12-04 16:47:43 +0300

    Script for automatic generic event generating
    <==== Mercurial data ====>
    Branch: master

---
+++ util/generic_event.py
@@ -0,0 +1,30 @@
+import requests
+import argparse
+from time import sleep
+
+default_timeout = 60
+
+def send_request():
+    try:
+        r = requests.get('http://localhost:7001/api/createEvent?caption=Test%20Caption&description=Test%20Description', auth=('admin', 'admin'), timeout=2)
+        print ("Success" if r.status_code == 200 else "Error")
+    except:
+        print "Timeout"
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--timeout', type=int, help="Timeout (in seconds), default value is 60")
+    args = parser.parse_args()
+    
+    timeout = args.timeout
+    if not timeout:
+        timeout = default_timeout
+       
+    print "Generate event every {0} seconds".format(timeout)
+
+    while True:
+        send_request()
+        sleep(timeout);
+
+if __name__ == '__main__':
+    main()
