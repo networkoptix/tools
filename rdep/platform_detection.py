1293e30a4b9fd0a050a5406fae8a8c2421a186a6
Denis Klychkov <dklychkov@networkoptix.com>
2016-02-25 13:16:48 +0300

    Add platform detection module.
    <==== Mercurial data ====>
    Branch: master

---
+++ rdep/platform_detection.py
@@ -0,0 +1,42 @@
+import platform
+import sys
+
+supported_platforms = [
+    "windows",
+    "linux",
+    "macos",
+    "android",
+    "ios"
+]
+
+supported_arches = [
+    "x64",
+    "x86",
+    "arm"
+]
+
+supported_boxes = [
+    "default",
+    "rpi",
+    "bpi",
+    "isd",
+    "isd_s2"
+]
+
+def detect_platform():
+    platform = sys.platform
+    if platform.startswith("win32") or platform.startswith("cygwin"):
+        return "windows"
+    elif platform.startswith("linux"):
+        return "linux"
+    elif platform.startswith("darwin"):
+        return "macos"
+    else:
+        return "unknown"
+
+def detect_arch():
+    arch = platform.machine()
+    if arch == "x86_64":
+        return "x64"
+
+    return arch
