5e98f6628965b2f40b699011cba972860520553a
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2016-03-29 14:19:00 +0300

    Their code is moved into ctl.sh
    <==== Mercurial data ====>
    Branch: master

--- testing/vagrant/str_init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /vagrant/conf.sh
-cp "${SERVCONF}.orig" "$SERVCONF"
-edconf removeDbOnStartup 1
-edconf logLevel DEBUG2
-edconf http-log-level DEBUG2
-# we have to set the password each time!
-#setpw
