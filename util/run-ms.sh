69bc83bf74474cad01b48cd7173f21b3e7910a7f
Mikhail Uskov <muskov@networkoptix.com>
2016-05-17 14:48:10 +0300

    util/run scripts minor improvements
    <==== Mercurial data ====>
    Branch: master

--- util/run-ms.sh
+++ util/run-ms.sh
@@ -6,7 +6,8 @@ cat <<END
 Runs Mediaserver with comfiguration over run.sh
 Usage: [OPTION=VALUE ...] run-ms.sh [<hex-id>] [ms-extra-args]
 Options:
-    DIR config directory to use, default $HOME/develop/mediaserver<hex-id>
+    DIR - config directory to use, default $HOME/develop/mediaserver<hex-id>
+    LL  - log level to use (DEBUG2 is default)
 END
 exit 0
 fi
@@ -17,14 +18,13 @@ ID=${1:-0}
 if [ $1 ]; then shift; fi
 
 DIR=${DIR:-$HOME/develop/mediaserver$ID}
-RUN=$(dirname "${BASH_SOURCE[0]}")/run.sh
+ARGS="-e --conf-file=$DIR/mediaserver.conf --runtime-conf-file=$DIR/run.conf"
 
-$RUN mediaserver -e $@ \
-   --conf-file=$DIR/mediaserver.conf \
-   --runtime-conf-file=$DIR/run.conf \
-   --log-level=DEBUG2 \
-   --http-log-level=DEBUG2 \
-   --ec2-tran-log-level=DEBUG2 \
-   --dev-mode-key=razrazraz \
-   --disable-crash-handler
+LL=${LL-DEBUG2}
+ARGS+=" --log-level=$LL --http-log-level=$LL --ec2-tran-log-level=$LL"
+
+if [ ! $noD ]; then ARGS+=" --dev-mode-key=razrazraz"; fi
+if [ ! $CH ]; then ARGS+=" --disable-crash-handler"; fi
+
+$(dirname "${BASH_SOURCE[0]}")/run.sh mediaserver $ARGS $@
 
