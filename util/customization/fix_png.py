d30cc2d0ed0b80fd234956e9617d4bde799659f0
Danil Lavrentyuk <dlavrentyuk@networkoptix.com>
2016-08-19 23:02:24 +0300

    Moidified to call multiple image processings in parallel
    <==== Mercurial data ====>
    Branch: master

--- util/customization/fix_png.py
+++ util/customization/fix_png.py
@@ -1,19 +1,44 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-#/bin/python
 
 import os
 import subprocess
+import time
 
-def system_call(args, cwd="."):
-    print("Running '{}' in '{}'".format(str(args), cwd))
-    subprocess.call(args, cwd=cwd)
-    pass
+command = ['pngcrush', '-ow', '-rem', 'allb', '-reduce']
+
+Processes = {}
+MaxProc = 20
+Quantum = 0.1
+
+
+def check_proc():
+    for key, proc in Processes.items():  # not .iteritems() - the dict is modified inside the loop
+        if proc.poll() is not None:
+            print "'{}' processing finished {}".format(
+                key, "OK" if proc.returncode == 0 else "with code {}".format(proc.returncode)
+            )
+            del Processes[key]
+
+
+def system_call(filename, cwd="."):
+    check_proc()
+    while len(Processes) >= MaxProc:
+        time.sleep(Quantum)
+        check_proc()
+    print("Processing '{}' in '{}'".format(filename, cwd))
+    Processes[filename] = subprocess.Popen(command + [filename], cwd=cwd)
+
 
 def fix_image_files(root=os.curdir):
     for dirname, dirnames, filenames in os.walk(root):
         for filename in filenames:
             if not filename.endswith('.png'):
                 continue
-            system_call("pngcrush -ow -rem allb -reduce \"{}\"".format(filename), dirname)
+            system_call(filename, dirname)
+    while Processes:
+        check_proc()
 
-fix_image_files()
+
+if __name__ == '__main__':
+    fix_image_files()
