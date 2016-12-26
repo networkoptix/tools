56e6a38bce6b159992bca79f3da95969648baf7f
Mike Shevchenko <mshevchenko@networkoptix.com>
2016-12-26 21:14:20 +0300

    patch-cloud-host: Minor refactoring and code improvement.
    Removed seearch for dll and lib because in Windows it is needed to patch either
    client or server 'exe'.
    
    <==== Mercurial data ====>
    Branch: master

--- util/patch-cloud-host.sh
+++ util/patch-cloud-host.sh
@@ -2,29 +2,46 @@
 
 CLOUD_HOST_KEY="this_is_cloud_host_name"
 FILE_LOCATION="build_environment"
-FILE_NAME=".*/\(lib\)?common\.\(a\|dll\|so\|lib\)"
-LIB_NAME="'common' library"
+FILE_PATH_REGEX=".*/libcommon\.\(a\|so\)"
 
 # From C++ source:
-CLOUD_HOST_NAME_WITH_KEY=$(eval echo "this_is_cloud_host_name cloud-test.hdw.mx\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
+CLOUD_HOST_NAME_WITH_KEY=$(eval echo \
+"this_is_cloud_host_name cloud-test.hdw.mx\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0")
 
 #--------------------------------------------------------------------------------------------------
 
-fail()
-{
-    echo "ERROR: $*" >&2
-    exit 1
-}
-
+# Allow 'set -x' to echo the args.
+# @param ...
 log()
 {
-    if [ "$VERBOSE" = "1" ]; then
-        echo "VERBOSE: $*"
+    # Args already echoed when processing this function call under 'set -x', thus, do nothing.
+    {
+        set +x;
+        [ ! -z "$VERBOSE" ] && set -x
+    } 2>/dev/null
+}
+
+# Echo the args replacing full home path with '~', unless the args are already echoed when
+# processing this function call under 'set -x'.
+writeln()
+{
+    { set +x; } 2>/dev/null
+    if [ -z "$VERBOSE" ]; then
+        echo "$@" |sed "s#$HOME/#~/#g"
+    else
+        set -x
     fi
 }
 
-# @return FILES Array of files found by 'find' command.
-find_array()
+# @param ...
+fail()
+{
+    writeln "ERROR: $@" >&2
+    exit 1
+}
+
+# @param[out] FILES Array of files found by 'find' command.
+find_FILES_array()
 {
     FILES=()
     while IFS= read -r -d $'\0'; do
@@ -32,106 +49,122 @@ find_array()
     done < <(find "$@" -print0)
 }
 
+#--------------------------------------------------------------------------------------------------
+
 # If not done yet, scan from current dir upwards to find root repository dir (e.g. develop/nx_vms).
-# [in][out] VMS_DIR
-find_vms_dir()
+# @param[in,out] VMS_DIR
+find_VMS_DIR()
 {
-    if [ "$VMS_DIR" != "" ]; then
-        return 1;
-    fi
+    [ "$VMS_DIR" != "" ] && return 1
 
     VMS_DIR=$(pwd)
     while [ $(basename $(dirname "$VMS_DIR")) != "develop" -a "$VMS_DIR" != "/" ]; do
         VMS_DIR=$(dirname "$VMS_DIR")
     done
 
-    if [ "$VMS_DIR" = "/" ]; then
-        fail "Run this script from any dir inside nx_vms."
+    [ "$VMS_DIR" = "/" ] && fail "Run this script from any dir inside nx_vms."
+}
+
+# Search for the file inside the given dir using the given filename regex via 'find'.
+# @param[out] FILE
+# @param FILE_LOCATION
+# @param FILE_PATH_REGEX
+find_FILE()
+{
+    local VMS_DIR
+    find_VMS_DIR
+
+    local -a FILES
+    find_FILES_array "$VMS_DIR/$FILE_LOCATION" -regex "$FILE_PATH_REGEX"
+
+    # Make sure 'find' returned exactly one file.
+    [ ${#FILES[*]} = 0 ] && fail "Unable to find the file to patch in $FILE_LOCATION" >&2
+    [ ${#FILES[*]} -gt 1 ] && fail "Found ${#FILES[*]} candidates to patch in $FILE_LOCATION" >&2
+    FILE=${FILES[0]}
+}
+
+# @param FILE
+save_backup()
+{
+    local BAK_FILE="$FILE.patch-cloud-host.BAK"
+    cp "$FILE" "$BAK_FILE" || fail "failed: cp $FILE $BAK_FILE"
+    writeln "Backup saved: $BAK_FILE"
+}
+
+# If no CLOUD_HOST_KEY, show the current text, otherwise, patch FILE to set the new text.
+# @param FILE
+# @param CLOUD_HOST_KEY
+# @param NEW_CLOUD_HOST
+process_file()
+{
+    local STRING=$(strings --radix=d -d "$FILE" |grep "$CLOUD_HOST_KEY")
+    [ -z "$STRING" ] && fail "'$CLOUD_HOST_KEY' string not found in $FILE"
+
+    local OFFSET=$(echo "$STRING" |awk '{print $1}')
+    local EXISTING_CLOUD_HOST=$(echo "$STRING" |awk '{print $3}')
+
+    if [ -z "$NEW_CLOUD_HOST" ]; then
+        writeln "Cloud Host is '$EXISTING_CLOUD_HOST' in $FILE"
+    elif [ "$NEW_CLOUD_HOST" = "$EXISTING_CLOUD_HOST" ]; then
+        writeln "Cloud Host is already '$EXISTING_CLOUD_HOST' in $FILE"
+    else
+        save_backup
+
+        local PATCH_OFFSET=$(expr "$OFFSET" + ${#CLOUD_HOST_KEY} + 1)
+        local NUL_OFFSET=$(expr "$PATCH_OFFSET" + ${#NEW_CLOUD_HOST})
+        local NUL_LEN=$(expr ${#CLOUD_HOST_NAME_WITH_KEY} - ${#CLOUD_HOST_KEY} - 1 - ${#NEW_CLOUD_HOST})
+        log "CLOUD_HOST_NAME_WITH_KEY len: ${#CLOUD_HOST_NAME_WITH_KEY}"
+        log "CLOUD_HOST_KEY len: ${#CLOUD_HOST_KEY}"
+
+        # Patching the printable chars.
+        echo -ne "$NEW_CLOUD_HOST" |dd bs=1 conv=notrunc seek="$PATCH_OFFSET" of="$FILE" \
+            2>/dev/null || fail "failed: dd of text"
+
+        # Filling the remaining bytes with NUL chars.
+        dd bs=1 conv=notrunc seek="$NUL_OFFSET" of="$FILE" if=/dev/zero count="$NUL_LEN" \
+            2>/dev/null || fail "failed: dd of NULs"
+
+        writeln "Cloud Host was '$EXISTING_CLOUD_HOST', now is '$NEW_CLOUD_HOST' in $FILE"
     fi
 }
 
-#--------------------------------------------------------------------------------------------------
+show_help_and_exit()
+{
+    writeln "Utility to patch 'exe' or compiled 'common' lib to replace Cloud Host."
+    writeln "Call from anywhere inside 'nx_vms' folder to search for the file."
+    writeln
+    writeln "Show current value:"
+    writeln "    $(basename $0) [--verbose] --show [path/to/binary_file]"
+    writeln
+    writeln "Patch with new value:"
+    writeln "    $(basename $0) [--verbose] 'new_cloud_host' [path/to/binary_file]"
+    exit 0
+}
 
 main()
 {
-    if [ $# = 0 -o "$1" = "-h" -o "$1" = "--help" ]; then
-        echo "Utility to patch the libcommon.so binary in order to replace Cloud Host."
-        echo
-        echo "Usage:"
-        echo
-        echo "Show current value:"
-        echo "$(basename $0) [--log] --show [path/to/libcommon.so]"
-        echo
-        echo "Patch with new value:"
-        echo "$(basename $0) [--log] 'new_cloud_group' [path/to/libcommon.so]"
-        exit 0
-    fi
+    [ $# = 0 -o "$1" = "-h" -o "$1" = "--help" ] && show_help_and_exit
 
+    local VERBOSE
     if [ "$1" == "--verbose" ]; then
         VERBOSE="1"
+        set -x
         shift
     fi
 
-    if [ "$1" != "--show" ]; then
-        NEW_CLOUD_HOST="$1"
-    fi
+    local NEW_CLOUD_HOST
+    [ "$1" != "--show" ] && NEW_CLOUD_HOST="$1"
 
-    # Determine FILE either from argv[2] or by searching with 'find'.
+    # Determine the file to patch either from argv[2] or by searching.
+    local FILE
     if [ $# -ge 2 ]; then
         FILE="$2"
-        if [ ! -f "$FILE" ]; then
-            fail "Specified file does not exist: $FILE"
-        fi
+        [ ! -f "$FILE" ] && fail "Specified file does not exist: $FILE"
     else
-        find_vms_dir
-
-        find_array "$VMS_DIR/$FILE_LOCATION" -regex "$FILE_NAME"
-
-        # Make sure 'find' returned exactly one file.
-        if [ ${#FILES[*]} = 0 ]; then
-            fail "Unable to find $LIB_NAME in $FILE_LOCATION." >&2
-        fi
-        if [ ${#FILES[*]} -gt 1 ]; then
-            fail "Found ${#FILES[*]} candidates of $LIB_NAME in $FILE_LOCATION." >&2
-        fi
-        FILE=${FILES[0]}
+        find_FILE
     fi
 
-    STRING=$(strings --radix=d -d "$FILE" |grep "$CLOUD_HOST_KEY")
-    if [ -z "$STRING" ]; then
-        fail "'$CLOUD_HOST_KEY' string not found in $FILE"
-    fi
-
-    OFFSET=$(echo "$STRING" |awk '{print $1}')
-    EXISTING_CLOUD_HOST=$(echo "$STRING" |awk '{print $3}')
-
-    log "Offset: $OFFSET, existing cloud host: $EXISTING_CLOUD_HOST"
-
-    if [ -z "$NEW_CLOUD_HOST" ]; then
-        echo "Cloud host: '$EXISTING_CLOUD_HOST' in $(readlink -f "$FILE")".
-    else
-        BAK_FILE="$FILE.patch-cloud-host.BAK"
-        cp "$FILE" "$BAK_FILE"
-
-        PATCH_OFFSET=$(expr "$OFFSET" + ${#CLOUD_HOST_KEY} + 1)
-        NUL_OFFSET=$(expr "$PATCH_OFFSET" + ${#NEW_CLOUD_HOST})
-        NUL_LEN=$(expr ${#CLOUD_HOST_NAME_WITH_KEY} - ${#CLOUD_HOST_KEY} - 1 - ${#NEW_CLOUD_HOST})
-        log "Patch offset: $PATCH_OFFSET, NUL offset: $NUL_OFFSET, NUL len: $NUL_LEN,"
-        log "    total len: ${#CLOUD_HOST_NAME_WITH_KEY}, key len: ${#CLOUD_HOST_KEY}"
-
-        # Patching the printable chars.
-        echo -ne "$NEW_CLOUD_HOST" |dd bs=1 conv=notrunc seek="$PATCH_OFFSET" of="$FILE" 2>/dev/null \
-            || fail "dd 1 failed"
-
-        # Filling the remaining bytes with '\0'.
-        dd bs=1 conv=notrunc seek="$NUL_OFFSET" of="$FILE" if=/dev/zero count=1 2>/dev/null \
-            || fail "dd 2 failed"
-
-        echo "SUCCESS: Replaced Cloud Host '$EXISTING_CLOUD_HOST' with '$NEW_CLOUD_HOST' in:"
-        echo "$FILE"
-        echo "Backup saved:"
-        echo "$BAK_FILE"
-    fi
+    process_file
 }
 
 main "$@"
