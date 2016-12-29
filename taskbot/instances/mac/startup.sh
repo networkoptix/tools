d64bb56df3037cc383f54a53e2829dcc6e95e975
Artem Nikitin <anikitin@networkoptix.com>
2016-12-29 12:11:13 +0300

    Taskbot: startup scripts
    <==== Mercurial data ====>
    Branch: master

---
+++ taskbot/instances/mac/startup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#export TASKBOT_DEBUG_MODE=1
+cd $HOME/taskbot/devtools/taskbot/instances/mac
+
+screen -wipe
+screen -dmS taskbot -t main bash
+screen -S taskbot -X screen -t dev_3_0_0 ./run.sh config_dev_3_0_0.py
+# screen -S taskbot -X screen -t dev_3_0_0_gui ./run.sh config_dev_3_0_0_gui.py
+# screen -S taskbot -X screen -t release_3_0 ./run.sh config_release_3_0.py
+# screen -S taskbot -X screen -t prod_3_0_0 ./run.sh config_prod_3_0_0.py
+# screen -S taskbot -X screen -t dev_2_6 ./run.sh config_dev_2_6.py
+# screen -S taskbot -X screen -t prod_2_6 ./run.sh config_prod_2_6.py
