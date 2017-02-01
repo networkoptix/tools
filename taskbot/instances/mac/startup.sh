62450b2ed75c70aa8546c8f754b6e5b352e69119
Artem Nikitin <anikitin@networkoptix.com>
2017-02-01 18:02:06 +0300

    Taskbot: dev_3_0_0_cloud
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/mac/startup.sh
+++ taskbot/instances/mac/startup.sh
@@ -8,6 +8,7 @@ cd $HOME/taskbot/devtools/taskbot/instances/mac
 screen -wipe
 screen -dmS taskbot -t main bash
 screen -S taskbot -X screen -t dev_3_0_0 ./run.sh config_dev_3_0_0.py
+screen -S taskbot -X screen -t dev_3_0_0_cloud ./run.sh config_dev_3_0_0_cloud.py
 # screen -S taskbot -X screen -t dev_3_0_0_gui ./run.sh config_dev_3_0_0_gui.py
 # screen -S taskbot -X screen -t release_3_0 ./run.sh config_release_3_0.py
 # screen -S taskbot -X screen -t prod_3_0_0 ./run.sh config_prod_3_0_0.py
--- taskbot/instances/windows/start.sh
+++ taskbot/instances/windows/start.sh
@@ -8,6 +8,7 @@ screen -dmS taskbot -t main bash
 screen -S taskbot -X screen -t dev_3_0_0 ./run.sh config_dev_3_0_0.py
 screen -S taskbot -X screen -t dev_3_0_0_gui ./run.sh config_dev_3_0_0_gui.py
 screen -S taskbot -X screen -t release_3_0 ./run.sh config_release_3_0.py
+screen -S taskbot -X screen -t dev_3_0_0_cloud ./run.sh config_dev_3_0_0_cloud.py
 # screen -S taskbot -X screen -t prod_3_0_0 ./run.sh config_prod_3_0_0.py
 # screen -S taskbot -X screen -t dev_2_6 ./run.sh config_dev_2_6.py
 # screen -S taskbot -X screen -t prod_2_6 ./run.sh config_prod_2_6.py
