e6fd6013d31ff057895f8513da84539a173f8f2b
Sergey Ivanov <sivanov@networkoptix.com>
2017-02-16 13:47:28 +0300

    Improved translation validation script
    <==== Mercurial data ====>
    Branch: master

--- util/translation/validation_rules.py
+++ util/translation/validation_rules.py
@@ -3,10 +3,14 @@
 class Levels:
     CRITICAL = "critical"
 
-class ForbiddenSymbolsRule:
+class ValidationRule:
     def __init__(self):
         self.lastErrorText = ""
 
+    def last_error_text(self):
+        return self.lastErrorText
+
+class ForbiddenSymbolsRule(ValidationRule):
     def __str__(self):
         return "Check forbidden symbols"
 
@@ -17,19 +21,41 @@ class ForbiddenSymbolsRule:
         return Levels.CRITICAL
 
     def valid(self, source, translation):
-        invalid = ['  ', '\'', '&apos;', 'href', '<html', '<br>', '<br/>']
-        for substring in invalid:
+        forbidden = ['  ', '&apos;', 'href', '<html', '<br>', '<br/>']
+        for substring in forbidden:
             if substring in source:
                 self.lastErrorText = u"Invalid substring {0} found in:\n\"{1}\"".format(substring, source)
                 return False
-                
+
         return True
+
+class ContractionsRule(ValidationRule):
+    def __str__(self):
+        return "Check contractions"
+
+    def __repr__(self):
+        return "<ContractionsRule>"
+
+    def level(self):
+        return Levels.CRITICAL
+
+    def valid(self, source, translation):
+        apos = '\''
+        if not apos in source:
+            return True
+
+        for word in (w for w in source.split(' ') if apos in w):
+            if word.endswith(apos) or word.endswith('\'s'):
+                continue
+            self.lastErrorText = u"Invalid contraction found in:\n\"{0}\"".format(source)
+            return False
         
-    def last_error_text(self):
-        return self.lastErrorText
+        return True
+
 
 def get_validation_rules():
     yield ForbiddenSymbolsRule()
+    yield ContractionsRule()
 
 if __name__ == "__main__":
     for rule in get_validation_rules():
