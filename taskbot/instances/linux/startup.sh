bb507880f35ccb71631a5a120ff4785ab53e13b1
Artem Nikitin <anikitin@networkoptix.com>
2017-02-21 08:53:04 +0300

    UT-41
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/linux/startup.sh
+++ taskbot/instances/linux/startup.sh
@@ -12,4 +12,4 @@ screen -S taskbot -X screen -t dev_3_0_0_gui ./run.sh config_dev_3_0_0_gui.py
 screen -S taskbot -X screen -t release_3_0 ./run.sh config_release_3_0.py
 screen -S taskbot -X screen -t prod_3_0_0 ./run.sh config_prod_3_0_0.py
 screen -S taskbot -X screen -t dev_3_0_0_cloud ./run.sh config_dev_3_0_0_cloud.py
-screen -S taskbot -X screen -t prod_3_0_0 ./run.sh config_default.py
+screen -S taskbot -X screen -t default ./run.sh config_default.py
