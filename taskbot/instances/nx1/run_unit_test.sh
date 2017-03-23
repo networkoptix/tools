76611191c27efe10928c696f7ffb7d2635319626
Artem Nikitin <anikitin@networkoptix.com>
2017-03-23 17:01:31 +0300

    Taskbot: NX1 disable mediaserver_core_ut logs
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/nx1/run_unit_test.sh
+++ taskbot/instances/nx1/run_unit_test.sh
@@ -4,13 +4,21 @@ TEST=$1
 TEST_VAR=../../var/$TEST
 
 mkdir -p $TEST_VAR
-export LD_LIBRARY_PATH=../lib:/opt/networkoptix/lib:/opt/networkoptix/mediaserver/lib
+BIN_PATH=$(pwd)
+LIB_PATH=$BIN_PATH/../lib
+export LD_LIBRARY_PATH=$LIB_PATH:/opt/networkoptix/lib:/opt/networkoptix/mediaserver/lib
 
 ulimit -c unlimited
 ulimit -n 4000
 
-./$TEST --gtest_filter=-NxCritical.All3 \
-        --gtest_shuffle \
-        --tmp=$TEST_VAR \
-        --log-file=../../var/$TEST \
-        --log-level=DEBUG2
+if [ "$TEST" != "mediaserver_core_ut" ]; then
+  ./$TEST --gtest_filter=-NxCritical.All3 \
+          --gtest_shuffle \
+          --tmp=$TEST_VAR \
+          --log-file=../../var/$TEST \
+          --log-level=DEBUG2
+else
+  ./$TEST --gtest_filter=-NxCritical.All3 \
+          --gtest_shuffle \
+          --tmp=$TEST_VAR
+fi
