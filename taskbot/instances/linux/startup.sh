b6d4815c5776631acaca3f5170f3f53e3f914902
Artem Nikitin <anikitin@networkoptix.com>
2017-04-05 14:12:14 +0300

    Taskbot: vms_3.1_gui
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/linux/startup.sh
+++ taskbot/instances/linux/startup.sh
@@ -13,4 +13,5 @@ screen -S taskbot -X screen -t release_3_0 ./run.sh config_release_3_0.py
 screen -S taskbot -X screen -t dev_3_0_0_cloud ./run.sh config_dev_3_0_0_cloud.py
 screen -S taskbot -X screen -t default ./run.sh config_default.py
 screen -S taskbot -X screen -t prod_3.0.0 ./run.sh config_prod_3_0_0.py
+screen -S taskbot -X screen -t vms_3.1_gui ./run.sh config_vms_3_1_gui.py
 screen -S taskbot -X screen -t slow_dev_3_0_0 ./slow.sh config_dev_3_0_0.py
