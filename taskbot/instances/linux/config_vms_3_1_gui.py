f33102a7b3548e477d62357449944392783898c9
Artem Nikitin <anikitin@networkoptix.com>
2017-04-05 14:09:21 +0300

    Taskbot: vms_3.1_gui
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/linux/config_vms_3_1_gui.py
+++ taskbot/instances/linux/config_vms_3_1_gui.py
@@ -5,7 +5,7 @@
 
 import os
 
-TASKBOT_BRANCHNAME='dev_3.0.0'
+TASKBOT_BRANCHNAME='vms_3.1_gui'
 TASKBOT_ROOT = os.path.join(os.environ['HOME'], 'taskbot')
 TASKBOT_VAR = os.path.join(
   TASKBOT_ROOT, TASKBOT_BRANCHNAME)
@@ -13,21 +13,14 @@ TASKBOT_DEVTOOLS_ROOT = os.path.join(TASKBOT_ROOT, 'devtools')
 
 config = {
   'sh': '/bin/bash',
-  'run_timeout': 4 * 60 * 60,
+  'run_timeout': 2 * 60 * 60,
   'select_timeout': 20 * 60,
   'gzip_threshold': 128,
   'gzip_ratio': 0.9,
   'max_output_size': 3 * 1024 * 1024,
   'watchers': {
     'Artem Nikitin': 'anikitin@networkoptix.com',
-    'Roman Vasilenko': 'rvasilenko@networkoptix.com',
-    'Andrey Kolesnikov': 'akolesnikov@networkoptix.com'},
-  'ft_watchers': {
-    'Artem Nikitin': 'anikitin@networkoptix.com',
-    'Andrey Kolesnikov': 'akolesnikov@networkoptix.com',
-    'Vsevolod Fedorov': 'vfedorov@networkoptix.com',
-    'Alexandra Matveeva': 'amatveeva@networkoptix.com',
-    'Roman Vasilenko': 'rvasilenko@networkoptix.com'  },
+    'Sergey Ivanov' : 'sivanov@networkoptix.com'},
   'environment' : {
     'TASKBOT_BRANCHNAME': TASKBOT_BRANCHNAME,
     'TASKBOT_PUBLIC_HTML_HOST': 'demo.networkoptix.com:3580',
@@ -38,9 +31,6 @@ config = {
     'TASKBOT_REPO': TASKBOT_DEVTOOLS_ROOT,
     'TASKBOT_NX_VMS_REPO': 'ssh://hg@hdw.mx/nx_vms',
     'TASKBOT_DEVTOOLS_REPO': 'ssh://hg@hdw.mx/devtools',
-    'TASKBOT_UNIT_TESTS': 'common_ut appserver2_ut',
-    'TASKBOT_STORE_ARTEFACTS': 'true',
-    'TASKBOT_FUNC_TESTS_OLD': 'false',
-    'TASKBOT_NET_BASE': '120'
+    'TASKBOT_UNIT_TESTS': 'client_ut common_ut nx_fusion_ut vms_utils_ut utils_ut'
     }
   }
--- taskbot/instances/linux/startup.sh
+++ taskbot/instances/linux/startup.sh
@@ -12,7 +12,5 @@ screen -S taskbot -X screen -t dev_3_0_0_gui ./run.sh config_dev_3_0_0_gui.py
 screen -S taskbot -X screen -t release_3_0 ./run.sh config_release_3_0.py
 screen -S taskbot -X screen -t dev_3_0_0_cloud ./run.sh config_dev_3_0_0_cloud.py
 screen -S taskbot -X screen -t default ./run.sh config_default.py
-screen -S taskbot -X screen -t default ./run.sh config_dev_2_6.py
-screen -S taskbot -X screen -t default ./run.sh config_prod_2_6.py
-screen -S taskbot -X screen -t default ./run.sh config_prod_3_0_0.py
-screen -S taskbot -X screen -t default ./slow.sh config_dev_3_0_0.py
+screen -S taskbot -X screen -t prod_3.0.0 ./run.sh config_prod_3_0_0.py
+screen -S taskbot -X screen -t slow_dev_3_0_0 ./slow.sh config_dev_3_0_0.py
