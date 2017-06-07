1115df9faaa69c2c1f1775ebf779ef36b19ad865
Artem Nikitin <anikitin@networkoptix.com>
2017-06-07 09:18:36 +0300

    Taskbot: NX1 3.1
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/nx1/config_vms_3_1_dev.py
+++ taskbot/instances/nx1/config_vms_3_1_dev.py
@@ -5,15 +5,15 @@
 
 import os
 
-TASKBOT_BRANCHNAME='vms_3.0'
+TASKBOT_BRANCHNAME='vms_3.1_dev'
 TASKBOT_ROOT = os.path.join(os.environ['HOME'], 'taskbot')
 TASKBOT_VAR = os.path.join(
-  TASKBOT_ROOT, TASKBOT_BRANCHNAME)
+  TASKBOT_ROOT, TASKBOT_BRANCHNAME + '_nx1')
 TASKBOT_DEVTOOLS_ROOT = os.path.join(TASKBOT_ROOT, 'devtools')
 
 config = {
   'sh': '/bin/bash',
-  'run_timeout': 4 * 60 * 60,
+  'run_timeout': 2 * 60 * 60,
   'select_timeout': 20 * 60,
   'gzip_threshold': 128,
   'gzip_ratio': 0.9,
@@ -22,28 +22,19 @@ config = {
     'Artem Nikitin': 'anikitin@networkoptix.com',
     'Roman Vasilenko': 'rvasilenko@networkoptix.com',
     'Andrey Kolesnikov': 'akolesnikov@networkoptix.com'},
-  'ft_watchers': {
-    'Artem Nikitin': 'anikitin@networkoptix.com',
-    'Andrey Kolesnikov': 'akolesnikov@networkoptix.com',
-    'Vsevolod Fedorov': 'vfedorov@networkoptix.com',
-    'Alexandra Matveeva': 'amatveeva@networkoptix.com',
-    'Roman Vasilenko': 'rvasilenko@networkoptix.com'  },
   'environment' : {
     'TASKBOT_BRANCHNAME': TASKBOT_BRANCHNAME,
     'TASKBOT_PUBLIC_HTML_HOST': 'demo.networkoptix.com:3580',
     'TASKBOT_VAR': TASKBOT_VAR,
     'TASKBOT_BIN': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/core'),
-    'TASKBOT_SHARE': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/linux'),
+    'TASKBOT_SHARE': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/nx1'),
     'TASKBOT_COMMONS': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/commons'),
     'TASKBOT_REPO': TASKBOT_DEVTOOLS_ROOT,
     'TASKBOT_NX_VMS_REPO': 'ssh://hg@hdw.mx/nx_vms',
     'TASKBOT_DEVTOOLS_REPO': 'ssh://hg@hdw.mx/devtools',
     'TASKBOT_UNIT_TESTS': 'all',
-    'TASKBOT_STORE_ARTEFACTS': 'true',
-    'TASKBOT_FUNC_TESTS_OLD': 'true',
-    'TASKBOT_NET_BASE': '120',
-    'TASKBOT_CAMERA_ADDRESS': '10.1.5.35',
-    'TASKBOT_PYTEST_OPTIONS': "-m 'not testcam'",
-    'TASKBOT_VM_BASE_PORT': '17000'
+    'TASKBOT_NX_VMS_BUILD_FLAGS': "-Dbox=bpi -Darch=arm -Dutb",
+    'TASKBOT_NX1_ADDRESS': '10.1.5.161',
+    'TASKBOT_DESTINATION_HOST': 'NX1'
     }
   }
