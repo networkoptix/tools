42cdd7c0582e89f10b2a74177b18247db579cb63
Artem Nikitin <anikitin@networkoptix.com>
2017-06-14 09:26:09 +0300

    Taskbot: x_vms_6226 enable func tests
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/linux/config_x_vms_6226.py
+++ taskbot/instances/linux/config_x_vms_6226.py
@@ -35,7 +35,7 @@ config = {
     'TASKBOT_NX_VMS_REPO': 'ssh://hg@hdw.mx/nx_vms',
     'TASKBOT_DEVTOOLS_REPO': 'ssh://hg@hdw.mx/devtools',
     'TASKBOT_UNIT_TESTS': 'all',
-#    'TASKBOT_STORE_ARTEFACTS': 'true',
+    'TASKBOT_STORE_ARTEFACTS': 'true',
     'TASKBOT_CAMERA_ADDRESS': '10.1.5.35',
     'TASKBOT_PYTEST_OPTIONS': "-m 'not testcam'",
     'TASKBOT_VM_BASE_PORT': '21000'
