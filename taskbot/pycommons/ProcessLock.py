96aa728a8f89e8187f5af3935665ee54c552c454
Vsevolod Fedorov <vfedorov@networkoptix.com>
2017-06-21 11:05:48 +0300

    # Taskbot: check for errno when handling socket error - do not suppress unexpected errors
    <==== Mercurial data ====>
    Branch: master

--- taskbot/pycommons/ProcessLock.py
+++ taskbot/pycommons/ProcessLock.py
@@ -1,4 +1,6 @@
 import socket
+import errno
+
 
 def get_lock(lock_name):
     # Without holding a reference to our socket somewhere it gets garbage
@@ -7,6 +9,9 @@ def get_lock(lock_name):
     lock_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
     try:
         lock_socket.bind('\0' + lock_name)
-    except socket.error:
-        return False
+    except socket.error as x:
+        if x.errno == errno.EADDRINUSE:
+            return False
+        else:
+            raise
     return True
