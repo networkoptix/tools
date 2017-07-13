3f47bba17e63f4a4688d832ed85fc55066353922
Sergey Ivanov <sivanov@networkoptix.com>
2017-07-13 17:28:16 +0300

    VMS-6544: Change "Email" to "email"
    <==== Mercurial data ====>
    Branch: master

---
+++ translation/rules/lowercase_rule.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from validation_rule import ValidationRule, Levels
+
+words = ['Email']
+exceptions = ['Email Settings']
+
+class LowercaseRule(ValidationRule):
+    def __str__(self):
+        return "Check if given nouns are not uppercases when it is not needed."
+
+    def __repr__(self):
+        return "<LowercaseRule>"
+
+    def level(self):
+        return Levels.CRITICAL
+
+    def valid_text(self, text):
+        for msg in exceptions:
+            if msg in text:
+                return True
+    
+        source_words = ValidationRule.words(text)[1:]
+        
+        for word in words:
+            if not word in source_words:
+                continue
+
+            self.lastErrorText = u"Capitalized {0} found in: \"{1}\"".format(word, text)
+            return False
+
+        return True
