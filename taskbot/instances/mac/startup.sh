50801d3d6abbf7b4fcd038c3c51bbc4dfc1cf432
Vsevolod Fedorov <vfedorov@networkoptix.com>
2017-09-19 16:39:33 +0300

    Taskbot: add cloud_17.1 branch for windows, linux and mac
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/mac/startup.sh
+++ taskbot/instances/mac/startup.sh
@@ -10,7 +10,7 @@ screen -dmS taskbot -t main bash
 screen -S taskbot -X screen -t vms_3.0 ./run.sh config_vms_3_0.py
 screen -S taskbot -X screen -t vms_3.1 ./run.sh config_vms_3_1.py
 screen -S taskbot -X screen -t cloud_dev ./run.sh config_cloud_dev.py
-screen -S taskbot -X screen -t cloud_17.1 ./run.sh config_cloud_17.1.py
+screen -S taskbot -X screen -t cloud_17.1 ./run.sh config_cloud_17_1.py
 screen -S taskbot -X screen -t default ./run.sh config_default.py
 screen -S taskbot -X screen -t vms_3.1_gui ./run.sh config_vms_3_1_gui.py
 screen -S taskbot -X screen -t vms_3.1_dev ./run.sh config_vms_3_1_dev.py
