8f2d54ba79f8b5c1e8d68689a5a9fee4e172f033
Vsevolod Fedorov <vfedorov@networkoptix.com>
2017-09-19 16:36:29 +0300

    Taskbot: add cloud_17.1 branch for windows, linux and mac
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/linux/startup.sh
+++ taskbot/instances/linux/startup.sh
@@ -12,6 +12,7 @@ screen -S taskbot -X screen -t vms_3.1 ./run.sh config_vms_3_1.py
 screen -S taskbot -X screen -t vms_3.1.1 ./run.sh config_vms_3_1_1.py
 screen -S taskbot -X screen -t vms_3.1.2 ./run.sh config_vms_3_1_2.py
 screen -S taskbot -X screen -t cloud_dev ./run.sh config_cloud_dev.py
+screen -S taskbot -X screen -t cloud_17.1 ./run.sh config_cloud_17_1.py
 screen -S taskbot -X screen -t default ./run.sh config_default.py
 screen -S taskbot -X screen -t vms_3.1_gui ./run.sh config_vms_3_1_gui.py
 screen -S taskbot -X screen -t vms_3.1_dev ./run.sh config_vms_3_1_dev.py
---
+++ taskbot/instances/windows/config_cloud_17_1.py
@@ -0,0 +1,39 @@
+# -*- python -*-
+# $Id$
+# Artem V. Nikitin
+# Windows config
+
+import os
+
+TASKBOT_BRANCHNAME='cloud_17.1'
+TASKBOT_ROOT = os.path.join(os.environ['HOME'], 'taskbot')
+TASKBOT_VAR = os.path.join(
+  TASKBOT_ROOT, TASKBOT_BRANCHNAME)
+TASKBOT_DEVTOOLS_ROOT = os.path.join(TASKBOT_ROOT, 'devtools')
+
+config = {
+  'sh': '/bin/bash',
+  'run_timeout': 2 * 60 * 60,
+  'select_timeout': 20 * 60,
+  'gzip_threshold': 128,
+  'gzip_ratio': 0.9,
+  'max_output_size': 3 * 1024 * 1024,
+  'watchers': {
+    'Andrey Kolesnikov': 'akolesnikov@networkoptix.com',
+    'Mikhail Uskov': 'muskov@networkoptix.com',
+    },
+  'environment' : {
+    'CDB_PATH': '/cygdrive/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/cdb.exe',
+    'TASKBOT_BRANCHNAME': TASKBOT_BRANCHNAME,
+    'TASKBOT_PUBLIC_HTML_HOST': 'demo.networkoptix.com:3580',
+    'TASKBOT_VAR': TASKBOT_VAR,
+    'TASKBOT_BIN': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/core'),                            
+    'TASKBOT_SHARE': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/windows'),
+    'TASKBOT_COMMONS': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/commons'),
+    'TASKBOT_REPO': TASKBOT_DEVTOOLS_ROOT,
+    'TASKBOT_NX_VMS_REPO': 'ssh://hg@hdw.mx/nx_vms',
+    'TASKBOT_DEVTOOLS_REPO': 'ssh://hg@hdw.mx/devtools',
+    'TASKBOT_UNIT_TESTS': 'all'
+    }
+  }
+
