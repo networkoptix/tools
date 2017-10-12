e09eb39a2f151b20dd2062bd25f3589d4b2838b3
Vsevolod Fedorov <vfedorov@networkoptix.com>
2017-10-12 17:02:39 +0300

    Junk-shop build saver: fix datetime parsing; skip first commit
    <==== Mercurial data ====>
    Branch: master

--- ci/junk_shop/junk_shop/save_build_info.py
+++ ci/junk_shop/junk_shop/save_build_info.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 
 # pick changeset log for a run from mercurial, save to junk-shop database
-# hg log  --template '{node|short}|{date}|{author|person}|{author|email}|{desc|firstline|utf8}\n' --rev 5886b93ee64d..ce6f050b6b3a
 
 import argparse
 import subprocess
 from datetime import datetime, timedelta
-import pytz
+import dateutil.parser as dateutil_parser
 from pony.orm import db_session, select, desc
 from junk_shop.utils import DbConfig
 from junk_shop import models
 from junk_shop.capture_repository import BuildParameters, DbCaptureRepository
 
 
-HG_LOG_TEMPLATE = r'{node|short}|{date}|{author|person}|{author|email}|{desc|firstline}\n'
+HG_LOG_TEMPLATE = r'{node|short}|{date|isodatesec}|{author|person}|{author|email}|{desc|firstline}\n'
 
 
 def pick_last_revision(repository):
@@ -26,15 +25,6 @@ def pick_last_revision(repository):
            build.build_num < parameters.build_num).order_by(desc(1)).first()
     return prev_build.revision
 
-def make_datetime(timestamp, offset_sec):
-    offset = timedelta(seconds=offset_sec)
-    for tz in pytz.all_timezones:
-        tzinfo = pytz.timezone(tz)
-        dt = datetime.fromtimestamp(timestamp, tzinfo)
-        if dt.utcoffset() == offset:
-            return dt
-    assert False, 'Unusual time zone offset: %s' % offset
-
 def delete_build_changesets(build):
     select(cs for cs in models.BuildChangeSet if cs.build is build).delete()
 
@@ -42,10 +32,10 @@ def load_change_sets(repository, src_dir, build, prev_revision):
     rev_range = '%s..%s' % (prev_revision, build.revision)
     args = ['hg', 'log', '--template', HG_LOG_TEMPLATE, '--rev', rev_range]
     output = subprocess.check_output(args, cwd=src_dir)
-    for line in output.splitlines():
+    lines = output.splitlines()
+    for line in lines[1:]:  # first revision is the last one included to previous build
         revision, date_str, user, email, desc = line.split('|', 4)
-        timestamp, offset_sec = date_str.split('-')
-        date = make_datetime(float(timestamp), int(offset_sec))
+        date = dateutil_parser.parse(date_str)
         print revision, date, user, email, desc
         changeset = models.BuildChangeSet(
             build=build,
@@ -64,6 +54,8 @@ def main():
     parser.add_argument('--build-parameters', type=BuildParameters.from_string, metavar=BuildParameters.example,
                         help='Build parameters; project, branch and changeset are the (only) required ones')
     args = parser.parse_args()
+    for param in ['project', 'branch', 'build_num', 'revision']:
+        assert getattr(args.build_parameters, param), '%s build parameter is required' % param
     repository = DbCaptureRepository(args.db_config, args.build_parameters)
     with db_session:
         build = repository.produce_build()
