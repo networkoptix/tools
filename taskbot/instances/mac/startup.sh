6174fe5321ec109c418b8556e242442f9f79fb43
Vsevolod Fedorov <vfedorov@networkoptix.com>
2018-01-26 14:37:49 +0300

    Taskbot: remove 'vms' and 'default' branches
    <==== Mercurial data ====>
    Branch: master

--- taskbot/instances/mac/startup.sh
+++ taskbot/instances/mac/startup.sh
@@ -11,7 +11,5 @@ screen -S taskbot -X screen -t vms_3.0 ./run.sh config_vms_3_0.py
 screen -S taskbot -X screen -t vms_3.1 ./run.sh config_vms_3_1.py
 screen -S taskbot -X screen -t vms_3.1_release ./run.sh config_vms_3_1_release.py
 screen -S taskbot -X screen -t vms_3.1.1_release ./run.sh config_vms_3_1_1_release.py
-screen -S taskbot -X screen -t vms ./run.sh config_vms.py
 screen -S taskbot -X screen -t cloud_dev ./run.sh config_cloud_dev.py
 screen -S taskbot -X screen -t cloud_17.1 ./run.sh config_cloud_17_1.py
-screen -S taskbot -X screen -t default ./run.sh config_default.py
