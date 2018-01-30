0fa6f24423cc34e30571b991c5de09f12b6b5dbe
Vsevolod Fedorov <vfedorov@networkoptix.com>
2018-01-30 11:09:04 +0300

    Junk-shop: add list_projects, list_branches and kill_builds commands
    <==== Mercurial data ====>
    Branch: master

--- ci/junk_shop/junk_shop/webapp/commands.py
+++ ci/junk_shop/junk_shop/webapp/commands.py
@@ -1,8 +1,23 @@
-from pony.orm import db_session
+from pony.orm import db_session, select
 import click
 
 from junk_shop.webapp import app
 from .. import models
+
+
+@app.cli.command()
+@db_session
+def list_projects():
+    '''List projects'''
+    for project in models.Project.select().order_by(models.Project.id):
+        click.echo(project.name)
+
+@app.cli.command()
+@db_session
+def list_branches():
+    '''List Branches'''
+    for branch in models.Branch.select().order_by(models.Branch.id):
+        click.echo(branch.name)
 
 
 @app.cli.command()
@@ -16,3 +31,26 @@ def kill_branch(branch_name):
         return
     click.echo('Deleting brach id=%r' % branch.id)
     branch.delete()
+
+@app.cli.command()
+@click.argument('project_name')
+@click.argument('branch_name')
+@click.option('--dry-run/--no-dry-run')
+@db_session
+def kill_builds(project_name, branch_name, dry_run):
+    '''Remove builds belonging to particular project and branch, and it's runs and artifacts'''
+    project = models.Project.get(name=project_name)
+    if not project:
+        click.echo('No such project: %r' % project_name)
+        return
+    branch = models.Branch.get(name=branch_name)
+    if not branch:
+        click.echo('No such branch: %r' % branch_name)
+        return
+    click.echo('Deleting builds for project id=%r, brach id=%r' % (project.id, branch.id))
+    query = select(build for build in models.Build if build.project is project and build.branch is branch)
+    count = query.count()
+    click.echo('Will delete %d builds' % count)
+    if not dry_run:
+        query.delete()
+
