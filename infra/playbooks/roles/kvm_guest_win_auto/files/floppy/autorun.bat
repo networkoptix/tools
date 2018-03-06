14ea6acdb32d45ae5e976b30a59e0b1939cf53c6
Ivan Remizov <iremizov@networkoptix.com>
2018-03-06 15:31:38 +0300

    Add user and network setup for Win guests
    <==== Mercurial data ====>
    Branch: master

--- infra/playbooks/roles/kvm_guest_win_auto/files/floppy/autorun.bat
+++ infra/playbooks/roles/kvm_guest_win_auto/files/floppy/autorun.bat
@@ -3,4 +3,4 @@ REM When VM Base Image is built using this playbook (image_library) then once
 REM VM is started and user is logged in this script will be executed
 REM ============================================================================
 
-cmd.exe /c a:/00_entry.bat
+cmd.exe /c a:\00_entry.bat >  a:\autorun.log
