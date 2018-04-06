150776013aa6ac3c75bfc82a0f6fe48ef8652c24
Artem Nikitin <anikitin@networkoptix.com>
2018-04-06 09:42:24 +0300

    taskbot cloud_17_2 branch
    <==== Mercurial data ====>
    Branch: master

---
+++ taskbot/instances/linux/config_cloud_17_2.py
@@ -0,0 +1,43 @@
+
+# -*- python -*-
+# $Id$
+# Artem V. Nikitin
+# Linux config
+
+import os
+
+TASKBOT_BRANCHNAME='cloud_17.2'
+TASKBOT_ROOT = os.path.join(os.environ['HOME'], 'taskbot')
+TASKBOT_VAR = os.path.join(
+  TASKBOT_ROOT, TASKBOT_BRANCHNAME)
+TASKBOT_DEVTOOLS_ROOT = os.path.join(TASKBOT_ROOT, 'devtools')
+
+config = {
+  'sh': '/bin/bash',
+  'run_timeout': 4 * 60 * 60,
+  'select_timeout': 20 * 60,
+  'gzip_threshold': 128,
+  'gzip_ratio': 0.9,
+  'max_output_size': 3 * 1024 * 1024,
+  'watchers': {
+    'Roman Vasilenko': 'rvasilenko@networkoptix.com',
+    'Andrey Kolesnikov': 'akolesnikov@networkoptix.com',
+    },
+  'ft_watchers': {
+    'Vsevolod Fedorov': 'vfedorov@networkoptix.com',
+    'Andrey Kolesnikov': 'akolesnikov@networkoptix.com',
+    'Roman Vasilenko': 'rvasilenko@networkoptix.com',
+    },
+  'environment' : {
+    'TASKBOT_BRANCHNAME': TASKBOT_BRANCHNAME,
+    'TASKBOT_PUBLIC_HTML_HOST': 'taskbot.hdw.mx',
+    'TASKBOT_VAR': TASKBOT_VAR,
+    'TASKBOT_BIN': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/core'),                            
+    'TASKBOT_SHARE': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/linux'),
+    'TASKBOT_COMMONS': os.path.join(TASKBOT_DEVTOOLS_ROOT, 'taskbot/instances/commons'),
+    'TASKBOT_REPO': TASKBOT_DEVTOOLS_ROOT,
+    'TASKBOT_NX_VMS_REPO': 'ssh://hg@hdw.mx/nx_vms',
+    'TASKBOT_DEVTOOLS_REPO': 'ssh://hg@hdw.mx/devtools',
+    'TASKBOT_UNIT_TESTS': 'all'
+    }
+  }
