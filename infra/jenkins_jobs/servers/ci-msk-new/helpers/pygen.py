4184f2e206dacd90488c1db34a7be1fd97353456
Ivan Remizov <iremizov@networkoptix.com>
2018-05-04 21:07:35 +0300

    Remove edge1 from all customizations, keep only for DW
    <==== Mercurial data ====>
    Branch: master

--- infra/jenkins_jobs/servers/ci-msk-new/helpers/pygen.py
+++ infra/jenkins_jobs/servers/ci-msk-new/helpers/pygen.py
@@ -1,20 +1,22 @@
 # This is a helper utility to generate list of all sub jobs in multijob
-for x in ("linux-x64 linux-x86 bananapi bpi rpi edge1 "
+for platform in ("linux-x64 linux-x86 bananapi bpi rpi edge1 "
     "tx1 android-arm win-x64 win-x86 mac ios").split():
-    for y in ("default default_cn default_zh_CN cox "
+    for customization in ("default default_cn default_zh_CN cox "
         "digitalwatchdog digitalwatchdog_global hanwha "
         "ipera ionetworks nutech ras senturian systemk "
         "tricom ust vista vmsdemoblue vmsdemoorange").split():
 
         # below is template for skipping non-desired intersections.
-        # if x == "--1--" and y == "--2--": continue
+        # if platform == "--1--" and customization == "--2--": continue
+
+        if platform == "edge1" and customization != "digitalwatchdog": continue
 
         print '''
-        - name: '{pipeline}.{branch}.vms.installer.'''+x+'''.'''+y+'''.all'
+        - name: '{pipeline}.{branch}.vms.installer.'''+platform+'''.'''+customization+'''.all'
           kill-phase-on: NEVER
           enable-condition: >-
-            ("$PLATFORM     ").trim().split(",").contains("'''+x+'''") &&
-            ("$CUSTOMIZATION").trim().split(",").contains("'''+y+'''")
+            ("$PLATFORM     ").trim().split(",").contains("'''+platform+'''") &&
+            ("$CUSTOMIZATION").trim().split(",").contains("'''+customization+'''")
           predefined-parameters: |
             BUILD_IDENTITY=${counter_var_name}
             NX_VMS_COMMIT=$NX_VMS_COMMIT
