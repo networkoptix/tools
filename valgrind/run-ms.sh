4e2e5ec6d4fa2918770481f966b105a09fe33cd2
Mikhail Uskov <muskov@networkoptix.com>
2018-06-09 21:53:44 +0300

    valgrind/run-ms: add support for client
    <==== Mercurial data ====>
    Branch: master

--- valgrind/run-ms.sh
+++ valgrind/run-ms.sh
@@ -3,27 +3,42 @@ set -e
 
 TOOL=${1:-help}
 CUSTOMIZATION=$2
+WHAT_TO_TEST=${3:-mediaserver} #client or mediaserver
 
 SCRIPT_PATH=$(dirname "${BASH_SOURCE[0]}")
-MS_PATH=$(find /opt -type d -name mediaserver | grep /$CUSTOMIZATION | head -1)
-echo Mediaserver path: $MS_PATH
 
-LOG_FILE=valgrind-ms.${TOOL}.$(date +%s)
-ARGS=$($SCRIPT_PATH/args.sh $TOOL $LOG_FILE)
-
-export LD_LIBRARY_PATH=$MS_PATH/lib
-if [[ "$(uname -p)" == x86* ]]; then
-    MS_BIN_ORIGINAL=$MS_PATH/bin/mediaserver-bin
-else
-    MS_BIN_ORIGINAL=$MS_PATH/bin/mediaserver
+if [[ "$WHAT_TO_TEST" == "client" ]]; then
+    LOG_FILE=valgrind-cl.${TOOL}.$(date +%s)
+    CL_PATH=$(find /opt -type d | grep client | grep /$CUSTOMIZATION | head -1)
+    echo Client path: $CL_PATH
+    export LD_LIBRARY_PATH=$CL_PATH/*/lib
+    export DISPLAY=:0
+    if [[ "$(uname -p)" == x86* ]]; then
+        BIN_ORIGINAL=$CL_PATH/*/bin/client-bin
+        BIN_VALGRIND=$BIN_ORIGINAL
+    else
+        BIN_ORIGINAL=$CL_PATH/*/bin/client
+        BIN_VALGRIND=$BIN_ORIGINAL
+    fi
+elif [[ "$WHAT_TO_TEST" == "mediaserver" ]]; then
+    LOG_FILE=valgrind-ms.${TOOL}.$(date +%s)
+    MS_PATH=$(find /opt -type d -name mediaserver | grep /$CUSTOMIZATION | head -1)
+    echo Mediaserver path: $MS_PATH
+    export LD_LIBRARY_PATH=$MS_PATH/lib
+    if [[ "$(uname -p)" == x86* ]]; then
+        BIN_ORIGINAL=$MS_PATH/bin/mediaserver-bin
+        # Get rid of capabilities.
+        BIN_VALGRIND=$MS_PATH/bin/mediaserver-valgrind
+        cat $BIN_ORIGINAL > $BIN_VALGRIND
+        chmod 755 $BIN_VALGRIND
+    else
+        BIN_ORIGINAL=$MS_PATH/bin/mediaserver
+    fi
 fi
 
-# Get rid of capabilities.
-MS_BIN_VALGRIND=$MS_PATH/bin/mediaserver-valgrind
-cat $MS_BIN_ORIGINAL > $MS_BIN_VALGRIND
-chmod 755 $MS_BIN_VALGRIND
+ARGS=$($SCRIPT_PATH/args.sh $TOOL $LOG_FILE)
 
 echo Output redirect: $LOG_FILE.out
 set -x
-valgrind $ARGS $MS_BIN_VALGRIND -e >$LOG_FILE.out 2>&1
+valgrind $ARGS $BIN_VALGRIND -e >$LOG_FILE.out 2>&1
 
