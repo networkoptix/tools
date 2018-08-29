0475030026c2fc555c1368897b44af9c3f0aa42b
Artem Nikitin <anikitin@networkoptix.com>
2018-08-29 10:59:35 +0300

    CI-249: install dotnet 4.6.2
    <==== Mercurial data ====>
    Branch: master

--- infra/playbooks/roles/local/image_library/files/floppy/install_choco_packages.ps1
+++ infra/playbooks/roles/local/image_library/files/floppy/install_choco_packages.ps1
@@ -6,6 +6,7 @@ C:\ProgramData\chocolatey\choco.exe install git -y --version 2.16.2
 setx PATH "%PATH%;C:\Program Files\Git\bin"
 
 C:\ProgramData\chocolatey\choco.exe install dotnet4.6.1 --version 4.6.01055.20170308 -y --verbose
+C:\ProgramData\chocolatey\choco.exe install dotnet4.6.2 --version 4.6.01590.20170129 -y --verbose
 C:\ProgramData\chocolatey\choco.exe install visualstudio2017community --version 15.2.26430.20170605 -y --verbose
 C:\ProgramData\chocolatey\choco.exe install visualstudio2017-workload-nativedesktop --version 1.1.1 --package-parameters "--includeOptional" -y --verbose
 C:\ProgramData\chocolatey\choco.exe install nuget.commandline -y --verbose --version 4.5.1
