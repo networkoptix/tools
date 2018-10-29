efe128e73fb25d3556d3af508259c5afcafc9ea3
Artem Nikitin <anikitin@networkoptix.com>
2018-10-29 17:19:37 +0300

    CI-278: install requests 2.18.4 to build nodes
    <==== Mercurial data ====>
    Branch: master

--- infra/playbooks/roles/local/image_library/files/floppy/install_python_packages.ps1
+++ infra/playbooks/roles/local/image_library/files/floppy/install_python_packages.ps1
@@ -3,3 +3,4 @@ python a:\get-pip.py
 C:\Python27\python.exe a:\get-pip.py
 C:\Python27\Scripts\pip.exe install pyyaml
 C:\Python27\Scripts\pip.exe install virtualenv
+C:\Python27\Scripts\pip.exe install requests==2.18.4
