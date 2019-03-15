69300816fcff401b754965970631f451fae0bedd
Sergey Ivanov <sivanov@networkoptix.com>
2019-03-15 12:32:46 +0300

    Windows signing script: improve local test server configuration
    <==== Mercurial data ====>
    Branch: master

--- sign_over_http/run_server.bat
+++ sign_over_http/run_server.bat
@@ -1,2 +1,3 @@
 SET TEMP=c:/develop/devtools/sign_over_http/temp
-start python3 signing_server.py -s c:/develop/buildenv/packages/windows/signtool/bin -c c:/develop/devtools/sign_over_http/certs -l log.txt %*
+SET SIGNTOOL_DIRECTORY=%RDEP_PACKAGES_DIR%/windows/signtool/bin
+start python3 signing_server.py -s %SIGNTOOL_DIRECTORY% -c ./certs -l temp/log.txt %*
