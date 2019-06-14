1dd16f9380c2e3445c464d93d15001fa72491e88
Sergey Ivanov <sivanov@networkoptix.com>
2019-06-14 15:03:46 +0300

    Update opensource script to properly save source revisions and to strip ol history
    <==== Mercurial data ====>
    Branch: master

--- hgconvert/opensource/to_nx_open.sh
+++ hgconvert/opensource/to_nx_open.sh
@@ -4,6 +4,6 @@ HGRCPATH=/etc/mercurial/hgrc
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-hg convert nx_vms nx_open --filemap=to_nx_open.filemap --sourcesort
+hg convert nx_vms nx_open --filemap=to_nx_open.filemap --sourcesort --config convert.hg.saverev=True --config convert.hg.startrev=opensource
 
 cd nx_open; hg push -f || true
