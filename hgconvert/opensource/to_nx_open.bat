5a75d87e9c540d830ea324046801b0f410b1544d
Sergey Ivanov <sivanov@networkoptix.com>
2019-06-14 18:53:14 +0300

    Fix opensource initial revision
    <==== Mercurial data ====>
    Branch: master

--- hgconvert/opensource/to_nx_open.bat
+++ hgconvert/opensource/to_nx_open.bat
@@ -1,1 +1,1 @@
-hg convert nx_vms nx_open --filemap=to_nx_open.filemap --sourcesort --config convert.hg.saverev=True --config convert.hg.startrev=opensource
+hg convert nx_vms nx_open --filemap=to_nx_open.filemap --sourcesort --config convert.hg.saverev=True --config convert.hg.startrev=7dd2a70974b3
--- hgconvert/opensource/to_nx_open.sh
+++ hgconvert/opensource/to_nx_open.sh
@@ -4,6 +4,6 @@ HGRCPATH=/etc/mercurial/hgrc
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-hg convert nx_vms nx_open --filemap=to_nx_open.filemap --sourcesort --config convert.hg.saverev=True --config convert.hg.startrev=opensource
+hg convert nx_vms nx_open --filemap=to_nx_open.filemap --sourcesort --config convert.hg.saverev=True --config convert.hg.startrev=7dd2a70974b3
 
 cd nx_open; hg push -f || true
