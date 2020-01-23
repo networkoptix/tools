120885e735c53aaf4d05cb5bdb8198f5c075662d
Evgeniy Karpov <ekarpov@networkoptix.com>
2020-01-23 19:16:47 +0300

    apidoctool: HandlerRegistrationMatcher allows special names and names with slash at the end
    <==== Mercurial data ====>
    Branch: master

--- apidoctool/src/com/nx/apidoctool/HandlerRegistrationMatcher.java
+++ apidoctool/src/com/nx/apidoctool/HandlerRegistrationMatcher.java
@@ -21,18 +21,33 @@ public final class HandlerRegistrationMatcher implements RegistrationMatcher
         if (params != null)
             return new RegistrationMatch(params[0], null, null, null);
 
-        params = sourceCode.matchLine(line, regHandlerRegexForFunctorInvalidName);
+        params = sourceCode.matchLine(line, regHandlerRegexForFunctorWithSpecialName);
         if (params != null)
             return new RegistrationMatch(null, null, null, null);
+
+        params = sourceCode.matchLine(line, regHandlerRegexForFunctorWithInvalidName);
+        if (params != null)
+        {
+            System.out.println("WARNING: " + sourceCode.getFilename() + ":" + line + ": "
+                + "Invalid API function name.");
+            return new RegistrationMatch(null, null, null, null);
+        }
 
         return null;
     }
 
-    //--------------------------------------------------------------------------
+    //---------------------------------------------------------------------------------------------
 
     private static final Pattern regHandlerRegexForFunctor = Pattern.compile(
-        "\\s*reg\\(\"\\w+\\/([\\w]*)\".*");
+        "\\s*reg\\(\"\\w+\\/([\\w]*\\/?)\".*");
 
-    private static final Pattern regHandlerRegexForFunctorInvalidName = Pattern.compile(
+    /**
+     * Allow function registration with name that set as not string literal or string literal with
+     * special symbols. Such name is not intended to match function name in the apidoc comment.
+     */
+    private static final Pattern regHandlerRegexForFunctorWithSpecialName = Pattern.compile(
+        "\\s*reg\\((?:[^\"]|\"[\\w\\?\\:\\/]+\").*");
+
+    private static final Pattern regHandlerRegexForFunctorWithInvalidName = Pattern.compile(
         "\\s*reg\\(.*");
 }
