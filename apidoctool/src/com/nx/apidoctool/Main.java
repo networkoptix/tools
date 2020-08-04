8cc7f1e47718e964deb0238a7be849fadb62eee2
Evgeniy Karpov <ekarpov@networkoptix.com>
2020-08-04 14:22:03 +0300

    apidoctool: Enable assertions by default

--- apidoctool/src/com/nx/apidoctool/Main.java
+++ apidoctool/src/com/nx/apidoctool/Main.java
@@ -50,6 +50,7 @@ public class Main
 
     public static void main(String[] args)
     {
+        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
         try
         {
             final Params params = new Params();
