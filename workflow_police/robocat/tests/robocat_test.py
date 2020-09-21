c45162de659be35f72d11e8e6ddb5fe7e6db6e8e
Dmitry Liman <dliman@networkoptix.com>
2020-09-21 12:11:55 +0300

    ENV-296: Refactor tests, add test for failed pipeline

--- workflow_police/robocat/tests/robocat_test.py
+++ workflow_police/robocat/tests/robocat_test.py
@@ -6,7 +6,8 @@ from dataclasses import dataclass, field
 import robocat.merge_request_handler
 
 
-COMMIT = ("11", "msg1")
+DEFAULT_COMMIT = {"sha": "11", "message": "msg1"}
+COMMITS = dict()
 
 
 @dataclass
@@ -14,8 +15,8 @@ class MergeRequestStub():
     approved: bool = True
     has_conflicts: bool = False
     needs_rebase: bool = False
-    no_commits: bool = False
-    pipeline_status: str = "success"
+    commits: bool = field(default_factory=lambda: {DEFAULT_COMMIT["sha"]: DEFAULT_COMMIT["message"]})
+    pipelines_list: str = field(default_factory=lambda: [(DEFAULT_COMMIT["sha"], "success")])
 
     id: int = 7
     title: str = "Do Zorz at work"
@@ -27,16 +28,17 @@ class MergeRequestStub():
     rebased: bool = field(default=False, init=False)
     merged: bool = field(default=False, init=False)
 
+    def __post_init__(self):
+        COMMITS.update(self.commits)
+
     def approvals_left(self):
         return 0 if self.approved else 1
 
     def last_commit(self):
-        return COMMIT if not self.no_commits else None
+        return next(iter(self.commits.items()), None)
 
     def pipelines(self):
-        if not self.pipeline_status:
-            return []
-        return [{"id": 7, "sha": COMMIT[0], "status": self.pipeline_status}]
+        return [{"id": p[0], "sha": p[1][0], "status": p[1][1]} for p in enumerate(self.pipelines_list)]
 
     def add_comment(self, title, message, emoji=""):
         self.comments.append((title, message, emoji))
@@ -53,7 +55,7 @@ class MergeRequestStub():
         self.merged = True
 
     def play_latest_pipeline(self):
-        self.pipeline_status = "running"
+        self.pipelines_list[0] = (self.pipelines_list[0][0], "running")
 
 
 @pytest.fixture
@@ -61,7 +63,7 @@ def mr_handler(monkeypatch):
     handler = robocat.merge_request_handler.MergeRequestHandler(None)
 
     def stub_get_commit_message(sha):
-        return COMMIT[1]
+        return COMMITS[sha]
 
     monkeypatch.setattr(handler, "_get_commit_message", stub_get_commit_message)
     return handler
@@ -71,39 +73,43 @@ testdata = [
     (
         # MR not approved: -> nothing changed
         {"approved": False},
-        {"wip": False, "comments": 0, "rebased": False, "merged": False, "pipeline_status": "success"}),
+        {"pipeline_status": "success"}),
     (
         # MR has conflicts -> wip, comment
         {"has_conflicts": True},
-        {"wip": True, "comments": 1, "rebased": False, "merged": False, "pipeline_status": "success"}),
+        {"actions": ["wip"], "comments": 1, "pipeline_status": "success"}),
     (
         # MR don't have commits -> nothing changed
-        {"no_commits": True},
-        {"wip": False, "comments": 0, "rebased": False, "merged": False, "pipeline_status": "success"}),
+        {"commits": {}},
+        {"pipeline_status": "success"}),
     (
         # Pipeline in progress -> nothing changed
-        {"pipeline_status": "running"},
-        {"wip": False, "comments": 0, "rebased": False, "merged": False, "pipeline_status": "running"}),
+        {"pipelines_list": [(DEFAULT_COMMIT["sha"], "running")]},
+        {"pipeline_status": "running"}),
     (
         # No pipelines, needs rebase -> pipeline started, comment
-        {"needs_rebase": True, "pipeline_status": None},
-        {"wip": False, "comments": 1, "rebased": False, "merged": False, "pipeline_status": "running"}),
+        {"needs_rebase": True, "pipelines_list": [(DEFAULT_COMMIT["sha"], "skipped")]},
+        {"comments": 1, "pipeline_status": "running"}),
     (
         # No pipelines, no conflicts -> pipeline started, comment
-        {"pipeline_status": None},
-        {"wip": False, "comments": 1, "rebased": False, "merged": False, "pipeline_status": "running"}),
+        {"pipelines_list": [(DEFAULT_COMMIT["sha"], "skipped")]},
+        {"comments": 1, "pipeline_status": "running"}),
     (
         # Pipeline successfull, needs rebase -> rebased, not merged
-        {"needs_rebase": True, "pipeline_status": "success"},
-        {"wip": False, "comments": 0, "rebased": True, "merged": False, "pipeline_status": "success"}),
+        {"needs_rebase": True},
+        {"actions": ["rebased"], "pipeline_status": "success"}),
     (
         # Pipeline successfull, no conflicts -> merged, comment
-        {"pipeline_status": "success"},
-        {"wip": False, "comments": 1, "rebased": False, "merged": True, "pipeline_status": "success"}),
+        {},
+        {"actions": ["merged"], "comments": 1, "pipeline_status": "success"}),
     (
         # Pipeline failed -> wip, comment
-        {"pipeline_status": "failed"},
-        {"wip": True, "comments": 1, "rebased": False, "merged": False, "pipeline_status": "failed"})
+        {"commits": {"11": "same_msg", "22": "same_msg"}, "pipelines_list": [("11", "failed"), ("22", "skipped")]},
+        {"actions": ["wip"], "comments": 1, "pipeline_status": "failed"}),
+    (
+        # Pipeline with old sha failed -> pipeline started, comment
+        {"commits": {"11": "same_msg", "22": "same_msg"}, "pipelines_list": [("11", "skipped"), ("22", "failed")]},
+        {"comments": 1, "pipeline_status": "running"})
 ]
 
 
@@ -111,8 +117,8 @@ testdata = [
 def test_merge_request_handler(mr_handler, mr_state, expected_state):
     mr = MergeRequestStub(**mr_state)
     mr_handler.handle(mr)
-    assert expected_state["wip"] == mr.is_wip
-    assert expected_state["rebased"] == mr.rebased
-    assert expected_state["merged"] == mr.merged
-    assert expected_state["pipeline_status"] == mr.pipeline_status
-    assert expected_state["comments"] == len(mr.comments)
+    assert ("wip" in expected_state.get("actions", [])) == mr.is_wip
+    assert ("rebased" in expected_state.get("actions", [])) == mr.rebased
+    assert ("merged" in expected_state.get("actions", [])) == mr.merged
+    assert expected_state.get("comments", 0) == len(mr.comments)
+    assert expected_state["pipeline_status"] == mr.pipelines_list[0][1]
