5c0a029450d9c9f640364a6c122d1b3ab4bdcd34
Sergei Ivanov <sivanov@networkoptix.com>
2020-10-19 13:27:18 +0300

    CI-1004: Create pypi package for sign-over-http server

--- sign_over_http/readme.md
+++ sign_over_http/readme.md
@@ -1,3 +1,5 @@
+// Copyright 2018-present Network Optix, Inc. Licensed under MPL 2.0: www.mozilla.org/MPL/2.0/
+
 # Overview
 
 The **Signing server** is a web-service, intended to physically isolate all signing certificates
---
+++ sign_over_http/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nx-sign-over-http",
+    version="2.0.0",
+    author="Sergei Ivanov",
+    author_email="sivanov@networkoptix.com",
+    description="Standalone sign-over-http service",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.lan.hdw.mx/dev/tools/-/tree/master/sign_over_http",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'pyyaml',
+        'aiohttp'
+    ],
+    package_data={
+        'server': [
+            'config/log.yaml',
+            'config/openssl_handler/genkey.bat',
+            'config/openssl_handler/genkey.sh',
+            'config/signtool_handler/config.yaml',
+            'config/signtool_handler/genkey.bat',
+        ],
+    },
+)
