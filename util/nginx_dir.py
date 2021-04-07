bd8b7819766639c7cd33b247803230e6f48ec0cd
Mikhail Uskov <muskov@networkoptix.com>
2021-04-07 18:32:21 +0300

    util/nginx_dir.py: apperance enhancements

--- util/nginx_dir.py
+++ util/nginx_dir.py
@@ -16,10 +16,14 @@
 # ```
 #
 
+import math
 import os
+import re
 import stat
 import sys
 from datetime import datetime
+
+SIMPLE_COLUMNS = 4
 
 
 def join_paths(*args):
@@ -36,16 +40,23 @@ def size_units(size):
     return '%s %s' % (size, suffixes[index])
 
 
-def print_files(path_base, link_base, uri, mode='advanced'):
-    print('<html><body>')
-    print('<h1>%s</h1>' % link_base)
-    if mode == 'advanced':
-        print('<table style="font-family: monospace; border-spacing: 10px 0;">')
-        print('<tr style="font-weight: bold;"><td>Name</td><td>Modified</td><td>Size</td><td>Type</td></tr>')
+def numeric_key(text):
+    return [0] + [
+        int(part) if part.isdigit() else part
+        for part in re.split(r'([0-9]+)', text)]
 
+
+def print_files(path_base, link_base, uri, mode='advanced'):
     path = join_paths(path_base, uri[len(link_base):])
-    for name in sorted(list(os.listdir(path)), reverse=True):
-        if mode == 'advanced':
+    names = sorted(list(os.listdir(path)), key=numeric_key, reverse=True)
+
+    print('<html><body>')
+    print('<h1>%s</h1><hr>' % link_base)
+    print('<table style="font-family: monospace; border-spacing: 10px 0;">')
+
+    if mode == 'advanced':
+        print('<tr style="font-weight: bold;"><td>Name</td><td>Modified</td><td>Size</td><td>Type</td></tr>')
+        for name in names:
             stats = os.stat(join_paths(path, name))
             suffix = '/' if stat.S_ISDIR(stats.st_mode) else ''
             link = '<a href="%s/%s%s">%s</a>%s' % (link_base, name, suffix, name, suffix)
@@ -54,10 +65,17 @@ def print_files(path_base, link_base, uri, mode='advanced'):
             node_type = 'Directory' if stat.S_ISDIR(stats.st_mode) else 'File'
             print('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (
                 link, time, '-' if stat.S_ISDIR(stats.st_mode) else size, node_type))
-        else:
-            print('<a href="%s/%s">%s</a><br>' % (link_base, name, name))
+    else:
+        print('<tr>')
+        links = ['<a href="%s/%s">%s</a><br>' % (link_base, name, name) for name in names]
+        part = math.ceil(len(links) / SIMPLE_COLUMNS)
+        for i in range(SIMPLE_COLUMNS):
+            print('<td width="%s%%">\n%s\n</td>' % (
+                math.floor(100 / SIMPLE_COLUMNS), '\n'.join(links[part * i: part * (i + 1)])))
+        print('</tr>')
 
     print('</table>')
+    print('<hr><div style="color: gray;">muskov@networkoptix.com</div>')
     print('</body></html>')
 
 
