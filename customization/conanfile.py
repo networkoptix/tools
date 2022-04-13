bd8cded87a8d6113d3a843fb87a82a6bc29b7730
Denis Klychkov <dklychkov@networkoptix.com>
2022-04-13 01:38:51 -0700

    Simplity the Conan recipe for customization packages

--- customization/conanfile.py
+++ customization/conanfile.py
@@ -1,45 +1,30 @@
-#!/usr/bin/env python
-
 from conans import ConanFile, tools
 import os
 import sys
 
-class CustomizationPackConan(ConanFile):
-    name = "customization_pack"
+
+class CustomizationConan(ConanFile):
+    name = "customization"
     version = "1.0"
     description = "Zip package for customization assets."
-    url = "https://cloud-test.hdw.mx"
-    exports_sources = "*"
     options = {
         "customization": "ANY",
-        "draft": [True, False],
     }
     default_options = {
         "customization": "default",
-        "draft": False,
     }
+    exports_sources = "get_zip_from_cloud.py"
 
     def build(self):
         sys.path.append(".")
         import get_zip_from_cloud
 
-        username = os.getenv("CMS_PULLER_USERNAME")
-        password = os.getenv("CMS_PULLER_PASSWORD")
-
-        arguments = [
-            username,
-            password
-        ]
-
-        if self.options.draft:
-            arguments.append("--draft")
-
-        arguments += [
+        get_zip_from_cloud.main([
+            os.getenv("CMS_PULLER_USERNAME"),
+            os.getenv("CMS_PULLER_PASSWORD"),
             "type",
-            "--customization=" + str(self.options.customization),
-        ]
-
-        get_zip_from_cloud.main(arguments)
+            f"--customization={self.options.customization}",
+        ])
 
     def package(self):
         self.copy("package.zip", dst="", src=f"customization_pack-{self.options.customization}")
