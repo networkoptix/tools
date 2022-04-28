0805bf8fa573492c87218c99d5ea6c7d30b8557c
Denis Klychkov <dklychkov@networkoptix.com>
2022-04-27 23:57:01 -0700

    Get "draft" option back to customization Conan package

--- customization/conanfile.py
+++ customization/conanfile.py
@@ -9,9 +9,11 @@ class CustomizationConan(ConanFile):
     description = "Zip package for customization assets."
     options = {
         "customization": "ANY",
+        "draft": [True, False],
     }
     default_options = {
         "customization": "default",
+        "draft": False
     }
     exports_sources = "get_zip_from_cloud.py"
 
@@ -19,12 +21,9 @@ class CustomizationConan(ConanFile):
         sys.path.append(".")
         import get_zip_from_cloud
 
-        get_zip_from_cloud.main([
-            os.getenv("CMS_PULLER_USERNAME"),
-            os.getenv("CMS_PULLER_PASSWORD"),
-            "type",
-            f"--customization={self.options.customization}",
-        ])
+        get_zip_from_cloud.main((["--draft"] if self.options.draft else [])
+            + [os.getenv("CMS_PULLER_USERNAME"), os.getenv("CMS_PULLER_PASSWORD")]
+            + ["type", f"--customization={self.options.customization}"])
 
     def package(self):
         self.copy("package.zip", dst="", src=f"customization_pack-{self.options.customization}")
