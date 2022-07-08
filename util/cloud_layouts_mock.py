e22e1a5809c8eadcc3f84d7639781528a2ec3503
Sergei Ivanov <sivanov@networkoptix.com>
2022-07-08 10:08:33 +0300

    Add delete layouts functionality to the cloud layouts mock

--- util/cloud_layouts_mock.py
+++ util/cloud_layouts_mock.py
@@ -34,6 +34,7 @@ async def get_layout(file_path: str, matchPrefix: str | None = None):
         "message": f"{file_path} is not found"
     }
 
+
 @app.post("/docdb/v1/docs/{file_path:path}")
 async def save_layout(file_path: str, request: Request):
     data = await request.json()
@@ -41,7 +42,14 @@ async def save_layout(file_path: str, request: Request):
     full_path.parent.mkdir(parents=True, exist_ok=True)
     with open(full_path, "w") as file:
         json.dump(data, file)
-    return { "message": f"File saved to {full_path}" }
+    return {"message": f"File saved to {full_path}"}
+
+
+@app.delete("/docdb/v1/docs/{file_path:path}")
+async def delete_layout(file_path: str, request: Request):
+    full_path = storage / file_path
+    full_path.unlink()
+    return {"message": f"File {full_path} deleted"}
 
 
 if __name__ == "__main__":
