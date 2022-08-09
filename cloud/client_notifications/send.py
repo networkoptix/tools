f78b312a452bdde4d3b4fbdba411efe9a0d4d881
Roman Barsegian <rbarsegian@networkoptix.com>
2022-08-09 04:32:39 +0300

    Fix typo in variable name

--- cloud/client_notifications/send.py
+++ cloud/client_notifications/send.py
@@ -4,7 +4,7 @@ import requests.auth
 CLOUD_HOST = 'dev2.cloud.hdw.mx'
 
 SYSTEM_ID = '733126ed-982f-4176-acd4-a44d0a38a328'
-SYSTEM_AUTK_KEY = '872c8756-af59-4ba6-a390-92e40a5e0114'
+SYSTEM_AUTH_KEY = '872c8756-af59-4ba6-a390-92e40a5e0114'
 TARGET_USERS = ['rbarsegian+test200@networkoptix.com']
 
 
@@ -21,4 +21,4 @@ def send_notification(notification_dict, targets, system_id, system_auth_key):
 
 
 if __name__ == '__main__':
-    send_notification({'title': 'Hello', 'body': 'Hi there!'}, TARGET_USERS, SYSTEM_ID, SYSTEM_AUTK_KEY)
+    send_notification({'title': 'Hello', 'body': 'Hi there!'}, TARGET_USERS, SYSTEM_ID, SYSTEM_AUTH_KEY)
