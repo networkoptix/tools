9fea8812c485be220568596830a705e2d2b66ff0
Sergei Ivanov <sivanov@networkoptix.com>
2022-11-17 17:00:41 +0300

    Simplify auto-auth script for windows hardware signature initialization

--- sign_over_http/util/autosigning.py
+++ sign_over_http/util/autosigning.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python2
 
-# This script enters password for hardware signing token. It is supposed to be autorun on user autologin.
-# It allows entering password only once per server start.
+# This script enters password for hardware signing token.
+# It is supposed to be autorun on user autologin.
+# It enters password every time when authentication dialogue appears.
 
 # Requirements:
 # * Install:
@@ -16,8 +17,6 @@
 import os
 import sys
 import time
-import subprocess
-import shutil
 import threading
 
 import pywintypes
@@ -47,56 +46,22 @@ def enum_handler(hwnd, params):
     entered_count[0] += 1
     return True
 
-def clicker_thread(stop_flag, password, entered_count):
-    while not stop_flag[0]:
+def clicker_thread(password, entered_count):
+    while True:
         try:
             win32gui.EnumWindows(enum_handler, (password, entered_count))
             time.sleep(1)
         except pywintypes.error as x:
             print('Error enumerating windows:', x.winerror, x)
-            break
-            # raise x
-
-
 
 print('')
-print('Signing in at: %s (UTC+%d)' % (time.asctime(), -time.timezone/3600))
+print('Start to check the auth dialogue at: %s (UTC+%d)' % (time.asctime(), -time.timezone/3600))
 key_password = os.environ['KEY_PASSWORD']  # key is missing if not added from credentials
 
-signtool_path = subprocess.check_output(['where', 'signtool.exe'], shell=True).rstrip()
-print('Copying %r -> %r' % (signtool_path, SIGNED_FILE))
-shutil.copyfile(signtool_path, SIGNED_FILE)
-
-stop_flag = [False]
 entered_count = [0]
-clicker = threading.Thread(target=clicker_thread, args=(stop_flag, key_password, entered_count))
+clicker = threading.Thread(target=clicker_thread, args=(key_password, entered_count))
 clicker.start()
 print('Clicker thread is started.')
-
-print('Signing using signtool.exe:')
-sys.stdout.flush()
-subprocess.check_call(['signtool.exe', 'sign', '/a', SIGNED_FILE], shell=True)
-
-print('Stopping clicker thread...')
-stop_flag[0] = True
-clicker.join()
-print('Clicker thread is stopped.')
-print('Password was entered %d times' % entered_count[0])
-
-print('Checking signature:')
-p = subprocess.Popen(['signtool.exe', 'verify', '/v', SIGNED_FILE], stdout=subprocess.PIPE, shell=True)
-stdout, stderr = p.communicate()
-
-if stderr:
-    print('signtool verify returned stderr:')
-    print(stderr)
-    sys.exit(10)
-
-if SIGNATURE_MARK in stdout:
-    print('Signature is in place; all OK')
-else:
-    print('Signature is missing; signing is FAILED.')
-    sys.exit(10)
 
 if entered_count[0] > 0:
     # mark jenkins job yellow when password was actually required and entered
